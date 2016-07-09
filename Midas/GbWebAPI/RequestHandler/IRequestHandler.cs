dc35d336cbaad15fc8aee682814ec5db6a692736
merlotdude <merlotdude@outlook.com>
2016-07-09 12:20:58 -0700

    saving changes to API

---
+++ Midas/GbWebAPI/RequestHandler/IRequestHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Midas.GreenBill.BusinessObject;
+
+namespace Midas.GreenBill.Api
+{
+    public interface IRequestHandler<T> 
+    {
+        HttpResponseMessage GetGbObjects(HttpRequestMessage request);
+        HttpResponseMessage GetGbObjectById(HttpRequestMessage request, int id);
+        HttpResponseMessage GetGbObjectByName(HttpRequestMessage request, string name);
+        HttpResponseMessage CreateGbObject(HttpRequestMessage request, T gbObject);
+        HttpResponseMessage UpdateGbObject(HttpRequestMessage request, T gbObject);
+        HttpResponseMessage DeleteGbObject(HttpRequestMessage request, int id);
+        HttpResponseMessage ValidateUniqueName(HttpRequestMessage request, string name);
+
+    }
+}
