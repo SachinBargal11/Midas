dabbfe724b2f18febe5d7b3928cfecd9289b0f0b
mitali patel <mpatel@greenyourbills.com>
2016-07-28 14:49:37 +0530

    sachin-Signup form, changes in Appvalidator and small changes

--- web-ui/compiled/components/pages/signup.js
+++ web-ui/compiled/components/pages/signup.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core', '@angular/forms', '@angular/router', '../../utils/AppValidators', '../elements/loader', '../../services/authentication-service', 'angular2-notifications', '../../stores/session-store', '../../models/user'], function(exports_1, context_1) {
+System.register(['@angular/core', '@angular/forms', '@angular/router', 'ng2-bootstrap', '../../utils/AppValidators', '../elements/loader', '../../stores/session-store', '../../stores/notifications-store', 'primeng/primeng'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['@angular/core', '@angular/forms', '@angular/router', '../../ut
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, forms_1, router_1, AppValidators_1, loader_1, authentication_service_1, angular2_notifications_1, session_store_1, user_1;
+    var core_1, forms_1, router_1, ng2_bootstrap_1, AppValidators_1, loader_1, session_store_1, notifications_store_1, primeng_1;
     var SignupComponent;
     return {
         setters:[
@@ -23,32 +23,35 @@ System.register(['@angular/core', '@angular/forms', '@angular/router', '../../ut
             function (router_1_1) {
                 router_1 = router_1_1;
             },
+            function (ng2_bootstrap_1_1) {
+                ng2_bootstrap_1 = ng2_bootstrap_1_1;
+            },
             function (AppValidators_1_1) {
                 AppValidators_1 = AppValidators_1_1;
             },
             function (loader_1_1) {
                 loader_1 = loader_1_1;
             },
-            function (authentication_service_1_1) {
-                authentication_service_1 = authentication_service_1_1;
-            },
-            function (angular2_notifications_1_1) {
-                angular2_notifications_1 = angular2_notifications_1_1;
-            },
             function (session_store_1_1) {
                 session_store_1 = session_store_1_1;
             },
-            function (user_1_1) {
-                user_1 = user_1_1;
+            function (notifications_store_1_1) {
+                notifications_store_1 = notifications_store_1_1;
+            },
+            function (primeng_1_1) {
+                primeng_1 = primeng_1_1;
             }],
         execute: function() {
             SignupComponent = (function () {
-                function SignupComponent(fb, _authenticationService, _notificationsService, _sessionStore, _router) {
+                function SignupComponent(fb, _router, _notificationsStore, _sessionStore, 
+                    // private _usersStore: UsersStore,
+                    _elRef) {
                     this.fb = fb;
-                    this._authenticationService = _authenticationService;
-                    this._notificationsService = _notificationsService;
-                    this._sessionStore = _sessionStore;
                     this._router = _router;
+                    this._notificationsStore = _notificationsStore;
+                    this._sessionStore = _sessionStore;
+                    this._elRef = _elRef;
+                    // user = new User({});
                     this.options = {
                         timeOut: 3000,
                         showProgressBar: true,
@@ -56,48 +59,48 @@ System.register(['@angular/core', '@angular/forms', '@angular/router', '../../ut
                         clickToClose: false,
                         maxLength: 10
                     };
-                    this.signupForm = this.fb.group({
-                        name: ['', forms_1.Validators.required],
-                        email: ['', [forms_1.Validators.required, AppValidators_1.AppValidators.emailValidator]],
-                        mobileNo: ['', [forms_1.Validators.required, AppValidators_1.AppValidators.mobileNoValidator]],
-                        password: ['', forms_1.Validators.required],
-                        confirmPassword: ['', forms_1.Validators.required]
-                    }, { validator: AppValidators_1.AppValidators.matchingPasswords('password', 'confirmPassword') });
-                    this.signupFormControls = this.signupForm.controls;
+                    this.isSaveUserProgress = false;
+                    this.signupform = this.fb.group({
+                        userInfo: this.fb.group({
+                            firstname: ['', forms_1.Validators.required],
+                            middlename: ['', forms_1.Validators.required],
+                            lastname: ['', forms_1.Validators.required],
+                            gender: ['', forms_1.Validators.required],
+                            dob: ['', forms_1.Validators.required],
+                            userType: ['', forms_1.Validators.required]
+                        }),
+                        contact: this.fb.group({
+                            email: ['', [forms_1.Validators.required, AppValidators_1.AppValidators.emailValidator]],
+                            cellPhone: ['', [forms_1.Validators.required, AppValidators_1.AppValidators.mobileNoValidator]],
+                            homePhone: [''],
+                            workPhone: [''],
+                            faxNo: ['']
+                        }),
+                        address: this.fb.group({
+                            address1: ['', forms_1.Validators.required],
+                            address2: [''],
+                            city: ['', forms_1.Validators.required],
+                            zipCode: ['', forms_1.Validators.required],
+                            state: ['', forms_1.Validators.required],
+                            country: ['', forms_1.Validators.required]
+                        }),
+                        account: this.fb.group({
+                            accountName: ['', forms_1.Validators.required],
+                            password: ['', forms_1.Validators.required],
+                            confirmPassword: ['', forms_1.Validators.required]
+                        }, { validator: AppValidators_1.AppValidators.matchingPasswords('password', 'confirmPassword') })
+                    });
+                    this.userformControls = this.signupform.controls;
                 }
                 SignupComponent.prototype.ngOnInit = function () {
-                };
-                SignupComponent.prototype.register = function () {
-                    var _this = this;
-                    this.isSignupInProgress = true;
-                    var result;
-                    var user = new user_1.User({
-                        'name': this.signupForm.value.name,
-                        'email': this.signupForm.value.email,
-                        'phone': this.signupForm.value.mobileNo,
-                        'password': this.signupForm.value.password
-                    });
-                    result = this._authenticationService.register(user);
-                    result.subscribe(function (response) {
-                        _this._notificationsService.success('Welcome!', 'You have suceessfully registered!');
-                        setTimeout(function () {
-                            _this._router.navigate(['/login']);
-                        }, 3000);
-                    }, function (error) {
-                        _this.isSignupInProgress = false;
-                        _this._notificationsService.error('Oh No!', 'Unable to register user.');
-                    }, function () {
-                        _this.isSignupInProgress = false;
-                    });
                 };
                 SignupComponent = __decorate([
                     core_1.Component({
                         selector: 'signup',
                         templateUrl: 'templates/pages/signup.html',
-                        directives: [forms_1.FORM_DIRECTIVES, forms_1.REACTIVE_FORM_DIRECTIVES, router_1.ROUTER_DIRECTIVES, loader_1.LoaderComponent, angular2_notifications_1.SimpleNotificationsComponent],
-                        providers: [authentication_service_1.AuthenticationService, angular2_notifications_1.NotificationsService]
+                        directives: [forms_1.FORM_DIRECTIVES, forms_1.REACTIVE_FORM_DIRECTIVES, ng2_bootstrap_1.DROPDOWN_DIRECTIVES, router_1.ROUTER_DIRECTIVES, loader_1.LoaderComponent, primeng_1.Calendar, primeng_1.RadioButton]
                     }), 
-                    __metadata('design:paramtypes', [forms_1.FormBuilder, authentication_service_1.AuthenticationService, angular2_notifications_1.NotificationsService, session_store_1.SessionStore, router_1.Router])
+                    __metadata('design:paramtypes', [forms_1.FormBuilder, router_1.Router, notifications_store_1.NotificationsStore, session_store_1.SessionStore, core_1.ElementRef])
                 ], SignupComponent);
                 return SignupComponent;
             }());
--- web-ui/src/components/pages/signup.ts
+++ web-ui/src/components/pages/signup.ts
@@ -1,21 +1,30 @@
-import {Component, OnInit} from '@angular/core';
-import {FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, Validators, FormGroup, FormBuilder, AbstractControl} from '@angular/forms';
+import {Component, OnInit, ElementRef} from '@angular/core';
+import {FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, Validators, FormControl, FormGroup, FormBuilder, AbstractControl} from '@angular/forms';
 import {ROUTER_DIRECTIVES, Router} from '@angular/router';
+import {DROPDOWN_DIRECTIVES} from 'ng2-bootstrap';
 import {AppValidators} from '../../utils/AppValidators';
 import {LoaderComponent} from '../elements/loader';
-import {AuthenticationService} from '../../services/authentication-service';
-import {SimpleNotificationsComponent, NotificationsService} from 'angular2-notifications';
-import {SessionStore} from '../../stores/session-store';
+// import {UsersStore} from '../../stores/users-store';
 import {User} from '../../models/user';
+import {Contact} from '../../models/contact';
+import {Address} from '../../models/address';
+import $ from 'jquery';
+import {SessionStore} from '../../stores/session-store';
+import {NotificationsStore} from '../../stores/notifications-store';
+import {Notification} from '../../models/notification';
+import {SimpleNotificationsComponent, NotificationsService} from 'angular2-notifications';
+import Moment from 'moment';
+import {Calendar, RadioButton, SelectItem} from 'primeng/primeng';
 
 @Component({
     selector: 'signup',
     templateUrl: 'templates/pages/signup.html',
-    directives: [FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, ROUTER_DIRECTIVES, LoaderComponent, SimpleNotificationsComponent],
-    providers: [AuthenticationService, NotificationsService]
+    directives: [FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, DROPDOWN_DIRECTIVES, ROUTER_DIRECTIVES, LoaderComponent, Calendar, RadioButton]
 })
 
 export class SignupComponent implements OnInit {
+ 
+    // user = new User({});
     options = {
         timeOut: 3000,
         showProgressBar: true,
@@ -23,57 +32,93 @@ export class SignupComponent implements OnInit {
         clickToClose: false,
         maxLength: 10
     };
-    signupForm: FormGroup;
-    signupFormControls;
-    isSignupInProgress;
+    signupform: FormGroup;   
+    userformControls;
+    isSaveUserProgress = false;
+ 
     constructor(
         private fb: FormBuilder,
-        private _authenticationService: AuthenticationService,
-        private _notificationsService: NotificationsService,
+        private _router: Router,
+        private _notificationsStore: NotificationsStore,
         private _sessionStore: SessionStore,
-        private _router: Router
+        // private _usersStore: UsersStore,
+        private _elRef: ElementRef
     ) {
-        this.signupForm = this.fb.group({
-            name: ['', Validators.required],
-            email: ['', [Validators.required, AppValidators.emailValidator]],
-            mobileNo: ['', [Validators.required, AppValidators.mobileNoValidator]],
-            password: ['', Validators.required],
-            confirmPassword: ['', Validators.required]
-        }, { validator: AppValidators.matchingPasswords('password', 'confirmPassword') });
-        
-        this.signupFormControls = this.signupForm.controls;
+        this.signupform = this.fb.group({
+                   userInfo: this.fb.group({
+                                  firstname: ['', Validators.required],
+                                  middlename: ['', Validators.required],
+                                  lastname: ['', Validators.required],
+                                  gender: ['', Validators.required],
+                                  dob: ['', Validators.required],
+                                  userType: ['', Validators.required]
+                              }),
+                   contact: this.fb.group({           
+                                  email: ['', [Validators.required, AppValidators.emailValidator]],
+                                  cellPhone: ['', [Validators.required, AppValidators.mobileNoValidator]],
+                                  homePhone: [''],
+                                  workPhone: [''],
+                                  faxNo: ['']
+                              }),
+                   address: this.fb.group({           
+                                  address1: ['', Validators.required],
+                                  address2: [''],
+                                  city: ['', Validators.required],
+                                  zipCode: ['', Validators.required],
+                                  state: ['', Validators.required],
+                                  country: ['', Validators.required]
+                              }), 
+                   account: this.fb.group({
+                                  accountName: ['', Validators.required],
+                                  password: ['', Validators.required],
+                                  confirmPassword: ['', Validators.required]
+                              }, { validator: AppValidators.matchingPasswords('password', 'confirmPassword') })             
+        });
+      
+         this.userformControls = this.signupform.controls;
+         
     }
 
     ngOnInit() {
         
     }
 
-    register() {
-        this.isSignupInProgress = true;
-        var result;
-        var user = new User({
-            'name': this.signupForm.value.name,
-            'email': this.signupForm.value.email,
-            'phone': this.signupForm.value.mobileNo,
-            'password': this.signupForm.value.password
-        });
-        result = this._authenticationService.register(user);
 
+    // saveUser() {
+    //     this.isSaveUserProgress = true;
+    //     var result;
+    //     var user = new User({
+    //         'firstname': this.userform.value.firstname,
+    //         'lastname': this.userform.value.lastname,
+    //         'email': this.userform.value.email,
+    //         'mobileNo': this.userform.value.mobileNo,
+    //         'address': this.userform.value.address,
+    //         'dob': this.userform.value.dob,
+    //         'createdUser': this._sessionStore.session.user.id
+    //     });
+    //     result = this._usersStore.addUser(user);
+    //     result.subscribe(
+    //         (response) => {
+    //             var notification = new Notification({
+    //                 'title': 'User added successfully!',
+    //                 'type': 'SUCCESS',
+    //                 'createdAt': Moment()
+    //             });
+    //             this._notificationsStore.addNotification(notification);
+    //             this._router.navigate(['/users/add']);
+    //         },
+    //         (error) => {
+    //             var notification = new Notification({
+    //                 'title': 'Unable to add user.',
+    //                 'type': 'ERROR',
+    //                 'createdAt': Moment()
+    //             });
+    //             this._notificationsStore.addNotification(notification);
+    //         },
+    //         () => {
+    //             this.isSaveUserProgress = false;
+    //         });
 
-        result.subscribe(
-            (response) => {
-                this._notificationsService.success('Welcome!', 'You have suceessfully registered!');
-                setTimeout(() => {
-                    this._router.navigate(['/login']);
-                }, 3000);
-            },
-            error => {
-                this.isSignupInProgress = false;
-                this._notificationsService.error('Oh No!', 'Unable to register user.');
-            },
-            () => {
-                this.isSignupInProgress = false;
-            });
-    }
+    // }
 
 }
