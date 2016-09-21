7424694c5b1baf9df0085ee8a34f37634f7cd0e7
midas <mpatel@greenyourbills.com>
2016-09-21 11:05:08 +0530

    sachin added users statistics as separate component

--- web-ui/src/components/pages/users/user-statistics.ts
+++ web-ui/src/components/pages/users/user-statistics.ts
@@ -1,7 +1,14 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+
+import 'rxjs/add/operator/share';
+import 'rxjs/add/operator/map';
+import 'chart.js';
 import {UsersService} from '../../../services/users-service';
 import {SessionStore} from '../../../stores/session-store';
 import {UsersStore} from '../../../stores/users-store';
+import {AccountDetail} from '../../../models/account-details';
+import {NotificationsService} from 'angular2-notifications';
 
 
 @Component({
@@ -10,30 +17,58 @@ import {UsersStore} from '../../../stores/users-store';
 })
 
 export class UserStatisticsComponent {
-    data: any;
-
-    constructor(
-        private _usersStore: UsersStore,
-        private _usersService: UsersService,
-        private _sessionStore: SessionStore
-     ) {
-        let accountId = this._sessionStore.session.account_id;
-        let result = this._usersService.getUsers(accountId);
-
-        result.subscribe(
-            (response) => {
-                this.data = {
-                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                    datasets: [
-                        {
-                            label: 'Users',
-                            backgroundColor: '#42A5F5',
-                            borderColor: '#1E88E5',
-                            // data: [65, 59, 80, 81, 56, 55, 40]
-                            data: response
-                        }
-                    ]
-                };
-            });
-      }
+data: any;
+options: any;
+users: AccountDetail[];
+usersLoading;
+constructor(
+    private _router: Router,
+    private _usersStore: UsersStore,
+    private _usersService: UsersService,
+    private _sessionStore: SessionStore,
+    private _notificationsService: NotificationsService
+) {
+    this.usersLoading = true;
+    this._usersStore.getUsers().subscribe(users => {
+        this.users = users;
+        let groupedUsers: Array<any> = _.chain(this.users).groupBy(function (user) {
+            return user.toJS().user.createDate.format('Do MMM YY');
+        }).map(function (value: Array<any>, key: string) {
+            return {
+                x: moment(key, 'Do MMM YY'),
+                y: value.length
+            };
+        })
+        .value();
+        this.data.datasets[0].data = groupedUsers;
+    },
+        null,
+        () => {
+            this.usersLoading = false;
+        }
+    );
+    this.options = {
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    unit: 'week'
+                }
+            }]
+        }
+    };
+    this.data = {
+        datasets: [
+            {
+                label: 'Users',
+                backgroundColor: '#42A5F5',
+                borderColor: '#1E88E5',
+                data: []
+            }
+        ]
+    };
+}
+  newDate(days) {
+    return moment().add(days, 'd');
+  }
 }
