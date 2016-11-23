5ad17a69600e67825365c540b42aead37ce2e035
mangeshsingh <msingh@greenyourbills.com>
2016-11-23 10:50:16 +0530

    updates

---
+++ Midas/MIDAS.GBX/GBBusinessObjects/ErrorObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIDAS.GBX.BusinessObjects
+{
+    public class ErrorObject
+    {
+        public ErrorLevel ErrorLevel { get; set; }
+        public string ErrorMessage { get; set; }
+        public object errorObject { get; set; }
+    }
+}
+
+
+public enum ErrorLevel
+{
+    Warning=1,
+    Error=2,
+    Critical=3,
+    Validation=4,
+    Exception=5
+}
