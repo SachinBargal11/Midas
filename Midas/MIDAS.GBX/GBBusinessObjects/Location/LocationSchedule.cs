875304c475e4fc31287275f3b87a9665a4f530e4
mangeshsingh <msingh@greenyourbills.com>
2016-12-07 18:10:22 +0530

    user update

---
+++ Midas/MIDAS.GBX/GBBusinessObjects/Location/LocationSchedule.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIDAS.GBX.BusinessObjects
+{
+    public class LocationSchedule : GbObject
+    {
+        public Location _location;
+        public Location location
+        {
+            get
+            {
+                return this._location;
+            }
+            set
+            {
+                _location = value;
+            }
+        }
+        public Schedule _schedule;
+        public Schedule schedule
+        {
+            get
+            {
+                return this._schedule;
+            }
+            set
+            {
+                _schedule = value;
+            }
+        }
+
+        public override List<BusinessValidation> Validate<T>(T entity)
+        {
+            List<BusinessValidation> validations = new List<BusinessValidation>();
+            BusinessValidation validation = new BusinessValidation();
+            return validations;
+        }
+    }
+}
