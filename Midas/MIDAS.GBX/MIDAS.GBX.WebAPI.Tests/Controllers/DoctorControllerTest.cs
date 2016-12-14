0e8dd62946f9d36ee1f87330d691dffadd3e9b53
milindkate21 <mkate@greenyourbills.com>
2016-12-14 12:11:50 +0530

    new files

---
+++ Midas/MIDAS.GBX/MIDAS.GBX.WebAPI.Tests/Controllers/DoctorControllerTest.cs
@@ -0,0 +1,125 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MIDAS.GBX.WebAPI;
+using MIDAS.GBX.WebAPI.Controllers;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Reflection;
+using MIDAS.GBX.BusinessObjects;
+
+namespace MIDAS.GBX.WebAPI.Tests.Controllers
+{
+    
+    [TestClass]
+   public class DoctorControllerTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            DoctorController controller = new DoctorController();
+            Doctor doctor = new Doctor();
+            doctor.LicenseNumber = "abnbdbq122";
+            doctor.WCBAuthorization = "";
+            doctor.WcbRatingCode = "";
+            doctor.NPI = "";
+            doctor.TaxType = GBEnums.TaxType.Tax1;
+            doctor.Title = "abcd";
+            doctor.User = new User();
+
+            doctor.User.UserName = "milind.k@codearray.tech";
+            doctor.User.MiddleName = "Micheal";
+            doctor.User.FirstName = "MILIND";
+            doctor.User.LastName = "Kate";
+            doctor.User.Gender = GBEnums.Gender.Male;
+            doctor.User.UserType = GBEnums.UserType.Admin;
+            doctor.User.Status = GBEnums.UserStatus.Active;
+
+            doctor.User.ContactInfo = new ContactInfo();
+            doctor.User.ContactInfo.EmailAddress = "milind.k@codearray.tech";
+
+            controller.Post(doctor);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            DoctorController controller = new DoctorController();
+            Doctor doctor = new Doctor();
+            doctor.LicenseNumber = "abnbdbq122";
+            doctor.WCBAuthorization = "";
+            doctor.WcbRatingCode = "";
+            doctor.NPI = "";
+            doctor.TaxType = GBEnums.TaxType.Tax1;
+            doctor.Title = "abcd";
+            doctor.User = new User();
+
+            doctor.User.UserName = "milind.k@codearray.tech";
+            doctor.User.MiddleName = "Micheal";
+            doctor.User.FirstName = "MILIND";
+            doctor.User.LastName = "Kate";
+            doctor.User.Gender = GBEnums.Gender.Male;
+            doctor.User.UserType = GBEnums.UserType.Admin;
+            doctor.User.Status = GBEnums.UserStatus.Active;
+
+            doctor.User.ContactInfo = new ContactInfo();
+            doctor.User.ContactInfo.EmailAddress = "milind.k@codearray.tech";
+
+            controller.Put(doctor);
+        }
+
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            DoctorController controller = new DoctorController();
+            Doctor doctor = new Doctor();
+            doctor.LicenseNumber = "abnbdbq122";
+            doctor.WCBAuthorization = "";
+            doctor.WcbRatingCode = "";
+            doctor.NPI = "";
+            doctor.TaxType = GBEnums.TaxType.Tax1;
+            doctor.Title = "abcd";
+            doctor.User = new User();
+
+            doctor.User.UserName = "milind.k@codearray.tech";
+            doctor.User.MiddleName = "Micheal";
+            doctor.User.FirstName = "MILIND";
+            doctor.User.LastName = "Kate";
+            doctor.User.Gender = GBEnums.Gender.Male;
+            doctor.User.UserType = GBEnums.UserType.Admin;
+            doctor.User.Status = GBEnums.UserStatus.Active;
+
+            doctor.User.ContactInfo = new ContactInfo();
+            doctor.User.ContactInfo.EmailAddress = "milind.k@codearray.tech";
+
+            controller.Delete(doctor);
+        }
+
+        [TestMethod]
+        public void IsUniqueTest()
+        {
+            DoctorController controller = new DoctorController();
+            Doctor doctor = new Doctor();
+            doctor.LicenseNumber = "abnbdbq122";
+            doctor.WCBAuthorization = "";
+            doctor.WcbRatingCode = "";
+            doctor.NPI = "";
+            doctor.TaxType = GBEnums.TaxType.Tax1;
+            doctor.Title = "abcd";
+            doctor.User = new User();
+
+            doctor.User.UserName = "milind.k@codearray.tech";
+            doctor.User.MiddleName = "Micheal";
+            doctor.User.FirstName = "MILIND";
+            doctor.User.LastName = "Kate";
+            doctor.User.Gender = GBEnums.Gender.Male;
+            doctor.User.UserType = GBEnums.UserType.Admin;
+            doctor.User.Status = GBEnums.UserStatus.Active;
+
+            doctor.User.ContactInfo = new ContactInfo();
+            doctor.User.ContactInfo.EmailAddress = "milind.k@codearray.tech";
+
+            controller.IsUnique(doctor);
+        }
+    }
+}
---
+++ Midas/MIDAS.GBX/MIDAS.GBX.WebAPI.Tests/Controllers/OTPControllerTest.cs
@@ -0,0 +1,173 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MIDAS.GBX.WebAPI;
+using MIDAS.GBX.WebAPI.Controllers;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Reflection;
+using MIDAS.GBX.BusinessObjects;
+
+namespace MIDAS.GBX.WebAPI.Tests.Controllers
+{
+    [TestClass]
+    public class OTPControllerTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            OTPController controller = new OTPController();
+            OTP otp = new OTP();
+            otp.User = new User();
+            otp.User.UserName = "milind.k@codearray.tech";
+            otp.User.MiddleName = "Micheal";
+            otp.User.FirstName = "MILIND";
+            otp.User.LastName = "Kate";
+            otp.User.Gender = GBEnums.Gender.Male;
+            otp.User.UserType = GBEnums.UserType.Admin;
+            otp.User.Status = GBEnums.UserStatus.Active;
+
+            otp.User.ContactInfo = new ContactInfo();
+            otp.User.ContactInfo.EmailAddress = "milind.k@codearray.tech";
+
+            otp.Pin = 1233;
+            otp.OTP1=1234;
+            //otp.Validate=;
+
+            controller.Post(otp);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            OTPController controller = new OTPController();
+            OTP otp = new OTP();
+            otp.User = new User();
+            otp.User.UserName = "milind.k@codearray.tech";
+            otp.User.MiddleName = "Micheal";
+            otp.User.FirstName = "MILIND";
+            otp.User.LastName = "Kate";
+            otp.User.Gender = GBEnums.Gender.Male;
+            otp.User.UserType = GBEnums.UserType.Admin;
+            otp.User.Status = GBEnums.UserStatus.Active;
+
+            otp.User.ContactInfo = new ContactInfo();
+            otp.User.ContactInfo.EmailAddress = "milind.k@codearray.tech";
+
+            otp.Pin = 1233;
+            otp.OTP1 = 1234;
+            //otp.Validate=;
+
+            controller.Put(otp);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            OTPController controller = new OTPController();
+            OTP otp = new OTP();
+            otp.User = new User();
+            otp.User.UserName = "milind.k@codearray.tech";
+            otp.User.MiddleName = "Micheal";
+            otp.User.FirstName = "MILIND";
+            otp.User.LastName = "Kate";
+            otp.User.Gender = GBEnums.Gender.Male;
+            otp.User.UserType = GBEnums.UserType.Admin;
+            otp.User.Status = GBEnums.UserStatus.Active;
+
+            otp.User.ContactInfo = new ContactInfo();
+            otp.User.ContactInfo.EmailAddress = "milind.k@codearray.tech";
+
+            otp.Pin = 1233;
+            otp.OTP1 = 1234;
+            //otp.Validate=;
+
+            controller.Delete(otp);
+        }
+
+        [TestMethod]
+        public void IsUniqueTest()
+        {
+            OTPController controller = new OTPController();
+            OTP otp = new OTP();
+            otp.User = new User();
+            otp.User.UserName = "milind.k@codearray.tech";
+            otp.User.MiddleName = "Micheal";
+            otp.User.FirstName = "MILIND";
+            otp.User.LastName = "Kate";
+            otp.User.Gender = GBEnums.Gender.Male;
+            otp.User.UserType = GBEnums.UserType.Admin;
+            otp.User.Status = GBEnums.UserStatus.Active;
+
+            otp.User.ContactInfo = new ContactInfo();
+            otp.User.ContactInfo.EmailAddress = "milind.k@codearray.tech";
+
+            otp.Pin = 1233;
+            otp.OTP1 = 1234;
+            //otp.Validate=;
+
+            controller.IsUnique(otp);
+        }
+
+        [TestMethod]
+        public void GenerateOTPTest()
+        {
+            OTPController controller = new OTPController();
+            OTP otp = new OTP();
+            otp.User = new User();
+            otp.User.UserName = "milind.k@codearray.tech";
+            otp.User.MiddleName = "Micheal";
+            otp.User.FirstName = "MILIND";
+            otp.User.LastName = "Kate";
+            otp.User.Gender = GBEnums.Gender.Male;
+            otp.User.UserType = GBEnums.UserType.Admin;
+            otp.User.Status = GBEnums.UserStatus.Active;
+
+            otp.User.ContactInfo = new ContactInfo();
+            otp.User.ContactInfo.EmailAddress = "milind.k@codearray.tech";
+
+            otp.Pin = 1233;
+            otp.OTP1 = 1234;
+            //otp.Validate=;
+
+            controller.GenerateOTP(otp);
+        }
+
+        [TestMethod]
+        public void ValidateOTPTest()
+        {
+            OTPController controller = new OTPController();
+            ValidateOTP validate = new ValidateOTP();
+            validate.otp = new OTP();
+            validate.otp.User = new User();
+            validate.otp.User.UserName = "milind.k@codearray.tech";
+            validate.otp.User.MiddleName = "Micheal";
+            validate.otp.User.FirstName = "MILIND";
+            validate.otp.User.LastName = "Kate";
+            validate.otp.User.Gender = GBEnums.Gender.Male;
+            validate.otp.User.UserType = GBEnums.UserType.Admin;
+            validate.otp.User.Status = GBEnums.UserStatus.Active;
+
+            validate.otp.User.ContactInfo = new ContactInfo();
+            validate.otp.User.ContactInfo.EmailAddress = "milind.k@codearray.tech";
+
+            validate.otp.Pin = 1233;
+            validate.otp.OTP1 = 1234;
+            
+            validate.user = new User();
+            validate.user.UserName = "milind.k@codearray.tech";
+            validate.user.MiddleName = "Micheal";
+            validate.user.FirstName = "MILIND";
+            validate.user.LastName = "Kate";
+            validate.user.Gender = GBEnums.Gender.Male;
+            validate.user.UserType = GBEnums.UserType.Admin;
+            validate.user.Status = GBEnums.UserStatus.Active;
+
+            validate.user.ContactInfo = new ContactInfo();
+            validate.user.ContactInfo.EmailAddress = "milind.k@codearray.tech";
+            //validate.Validate=;
+            
+            controller.ValidateOTP(validate);
+        }
+
+    }
+}
