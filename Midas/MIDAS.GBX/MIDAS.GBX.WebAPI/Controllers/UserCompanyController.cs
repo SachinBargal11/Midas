26795caa768513006a66068d2d36856693574429
Mangesh <mangeshgangwar@gmail.com>
2016-12-19 15:05:24 +0530

    sunday bug fix

---
+++ Midas/MIDAS.GBX/MIDAS.GBX.WebAPI/Controllers/UserCompanyController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Linq;
+using MIDAS.GBX.BusinessObjects;
+using MIDAS.GBX.WebAPI.Filters;
+
+namespace MIDAS.GBX.WebAPI.Controllers
+{
+    [RoutePrefix("midasapi/UserCompany")]
+    public class UserCompanyController : ApiController
+    {
+
+        private IRequestHandler<UserCompany> requestHandler;
+        public UserCompanyController()
+        {
+            requestHandler = new GbApiRequestHandler<UserCompany>();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("GetAll")]
+        public HttpResponseMessage Get([FromBody]UserCompany data)
+        {
+            return requestHandler.GetGbObjects(Request, data);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("Get/{id}")]
+        public HttpResponseMessage Get(int id)
+        {
+            return requestHandler.GetObject(Request, id);
+        }
+
+        // POST: api/Organizations
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("Add")]
+        public HttpResponseMessage Post([FromBody]UserCompany data)
+        {
+            return requestHandler.CreateGbObject(Request, data);
+        }
+
+        // PUT: api/Organizations/5
+        [AllowAnonymous]
+        [Route("Update")]
+        [HttpPut]
+        public HttpResponseMessage Put([FromBody]UserCompany User)
+        {
+            return requestHandler.UpdateGbObject(Request, User);
+        }
+
+        // DELETE: api/Organizations/id={organizationId}
+        [AllowAnonymous]
+        [HttpDelete]
+        [Route("Delete")]
+        public HttpResponseMessage Delete([FromBody]UserCompany User)
+        {
+            return requestHandler.DeleteGbObject(Request, User);
+        }
+
+
+        // Unique Name Validation
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("IsUnique")]
+        public HttpResponseMessage IsUnique([FromBody]UserCompany User)
+        {
+            return requestHandler.ValidateUniqueName(Request, User);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+    }
+}
