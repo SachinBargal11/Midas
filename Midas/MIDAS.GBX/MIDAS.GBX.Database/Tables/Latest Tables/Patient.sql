82cfbb2cd7dd19d219f89d77d0fb654b92607390
satish.k <satish.k@codearray.tech>
2017-01-13 15:45:29 +0530

    Added new columns for patient portal

--- Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/Patient.sql
+++ Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/Patient.sql
@@ -9,6 +9,12 @@
 	[ChartNo] [nvarchar](50) NOT NULL,
 	[CompanyID] [int] NOT NULL,
 	[LocationID] [int] NOT NULL,
+	[Weight] [DECIMAL](3, 2) NOT NULL,
+	[MaritalStatus] [INT] NULL,
+	[DrivingLicence] [NVARCHAR](15) NULL,
+	[EmergenceyContact] [NVARCHAR](50) NULL,
+	[EmergenceyContactNumber] [NVARCHAR](50) NULL,
+	[EmergenceyContactRelation] [NVARCHAR](50) NULL,
 	[IsDeleted] [bit] NULL,
 	[CreateByUserID] [int] NOT NULL,
 	[CreateDate] [datetime2](7) NOT NULL,
@@ -37,5 +43,19 @@ REFERENCES [dbo].[User] ([id])
 GO
 
 ALTER TABLE [dbo].[Patient] CHECK CONSTRAINT [FK_Patient_User]
+GO
+
+ALTER TABLE [dbo].[Patient]  WITH CHECK ADD  CONSTRAINT [FK_Patient_MaritalStatus] FOREIGN KEY([MaritalStatus])
+REFERENCES [dbo].[MaritalStatus] ([id])
+GO
+
+ALTER TABLE [dbo].[Patient] CHECK CONSTRAINT [FK_Patient_MaritialStatus]
+GO
+
+ALTER TABLE [dbo].[User]  WITH CHECK ADD  CONSTRAINT [FK_User_UserType] FOREIGN KEY([UserType])
+REFERENCES [dbo].[UserType] ([id])
+GO
+
+ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_UserType]
 GO
 
