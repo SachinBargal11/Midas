00f118b9cf00fcb2d4347d6ca0f97eec7ca0bb5c
satish.k <satish.k@codearray.tech>
2017-01-13 13:47:30 +0530

    New SQL Tables

---
+++ Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/Gender.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Gender]
+(
+	[Id] INT NOT NULL,
+	[GenderText] [NVARCHAR](50) NOT NULL,
+	[IsDeleted] [BIT] NULL,
+	--[CreateByUserID] [INT] NOT NULL,
+	--[CreateDate] [DATETIME2](7) NOT NULL,
+	--[UpdateByUserID] [INT] NULL,
+	--[UpdateDate] [DATETIME2](7) NULL,
+    CONSTRAINT [PK_Gender] PRIMARY KEY ([id])
+)
+GO
---
+++ Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/MaritalStatus.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[MaritalStatus]
+(
+	[Id] [INT] NOT NULL,
+	[StatusText] [NVARCHAR](15) NOT NULL,
+	[IsDeleted] [BIT] NULL,
+	--[CreateByUserID] [INT] NOT NULL,
+	--[CreateDate] [DATETIME2](7) NOT NULL,
+	--[UpdateByUserID] [INT] NULL,
+	--[UpdateDate] [DATETIME2](7) NULL,
+	CONSTRAINT [PK_MaritalStatus] PRIMARY KEY ([id])
+)
+GO
