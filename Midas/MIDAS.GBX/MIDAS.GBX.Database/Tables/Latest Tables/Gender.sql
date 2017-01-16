5650d7e1cb4c4feb23278cfc5e316c4e66856fe1
satish.k <satish.k@codearray.tech>
2017-01-16 15:06:50 +0530

    Added new SQL table Gender

--- Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/Gender.sql
+++ Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/Gender.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [dbo].[Gender]
 (
-	[Id] INT NOT NULL,
+	[Id] [TINYINT] NOT NULL,
 	[GenderText] [NVARCHAR](50) NOT NULL,
 	[IsDeleted] [BIT] NULL,
 	--[CreateByUserID] [INT] NOT NULL,
 	--[CreateDate] [DATETIME2](7) NOT NULL,
 	--[UpdateByUserID] [INT] NULL,
 	--[UpdateDate] [DATETIME2](7) NULL,
-    CONSTRAINT [PK_Gender] PRIMARY KEY ([id])
+    CONSTRAINT [PK_Gender] PRIMARY KEY ([Id])
 )
 GO
