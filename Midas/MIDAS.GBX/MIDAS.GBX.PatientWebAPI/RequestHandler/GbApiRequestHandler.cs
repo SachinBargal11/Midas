bd5cf03162b37eafe24e6d1e345fd6039cbb3e2b
satish.k <satish.k@codearray.tech>
2017-01-17 18:44:31 +0530

    Implementing User Login from MIDAS.GBX.PatientWebAPI with the business logic and data access code reused as in MIDAS.GBX.WebAPI

---
+++ Midas/MIDAS.GBX/MIDAS.GBX.PatientWebAPI/RequestHandler/GbApiRequestHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using MIDAS.GBX.BusinessObjects;
+using MIDAS.GBX.DataAccessManager;
+
+namespace MIDAS.GBX.PatientWebAPI.RequestHandler
+{
+    public class GbApiRequestHandler<T> : IRequestHandler<T>
+    {
+        private IGbDataAccessManager<T> dataAccessManager;
+
+        public GbApiRequestHandler()
+        {
+            dataAccessManager = new GbDataAccessManager<T>();
+        }
+
+        public HttpResponseMessage Login(HttpRequestMessage request, T gbObject)
+        {
+            User userBO = (User)(object)gbObject;
+            if (userBO == null)
+            {
+                return request.CreateResponse(HttpStatusCode.BadRequest, new ErrorObject { ErrorMessage = "User object can't be null", errorObject = "", ErrorLevel = ErrorLevel.Error });
+            }
+            var objResult = dataAccessManager.Login(gbObject);
+
+            try
+            {
+                var res = (GbObject)(object)objResult;
+                return request.CreateResponse(HttpStatusCode.Created, objResult);
+
+            }
+            catch (Exception ex)
+            {
+                return request.CreateResponse(HttpStatusCode.BadRequest, objResult);
+            }
+        }
+    }
+}
---
+++ Midas/MIDAS.GBX/MIDAS.GBX.PatientWebAPI/RequestHandler/IRequestHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace MIDAS.GBX.PatientWebAPI.RequestHandler
+{
+    public interface IRequestHandler<T>
+    {
+        HttpResponseMessage Login(HttpRequestMessage request, T gbObject);
+    }
+}
