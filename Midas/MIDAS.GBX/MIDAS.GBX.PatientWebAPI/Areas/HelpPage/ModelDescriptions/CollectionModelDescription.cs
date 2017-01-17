081eb30c3eebb3f2a59c2448f3c1ff4a9f113b04
satish.k <satish.k@codearray.tech>
2017-01-17 17:21:52 +0530

    Added new WebAPI project for Patient Portal MIDAS.GBX.PatientWebAPI

---
+++ Midas/MIDAS.GBX/MIDAS.GBX.PatientWebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MIDAS.GBX.PatientWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Midas/MIDAS.GBX/MIDAS.GBX.PatientWebAPI/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace MIDAS.GBX.PatientWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
