88e9f983a0e036a414d72c4d5ae518cc4f7c3f9e
satish.k <satish.k@codearray.tech>
2017-01-25 18:26:59 +0530

    Added Validation Message for Patient

--- Midas/MIDAS.GBX/GBBusinessObjects/Patient/Patient.cs
+++ Midas/MIDAS.GBX/GBBusinessObjects/Patient/Patient.cs
@@ -11,7 +11,7 @@ namespace MIDAS.GBX.BusinessObjects
 {
     public class Patient : GbObject
     {
-
+        public int id { get; set; }
         public int PatientID { get; set; }
         public string SSN { get; set; }
         public string WCBNo { get; set; }
@@ -26,10 +26,113 @@ namespace MIDAS.GBX.BusinessObjects
         public Location Location { get; set; }
         public User User { get; set; }
 
+        public decimal Weight { get; set; }
+        public byte MaritalStatus { get; set; }
+        public string DrivingLicence { get; set; }
+        public string EmergenceyContact { get; set; }
+        public string EmergenceyContactNumber { get; set; }
+        public string EmergenceyContactRelation { get; set; }
+
         public override List<BusinessValidation> Validate<T>(T entity)
         {
             List<BusinessValidation> validations = new List<BusinessValidation>();
             BusinessValidation validation = new BusinessValidation();
+
+            if (id < 0)
+            {
+                validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "ID cannot be less than zero." });
+            }
+
+            if (PatientID < 0)
+            {
+                validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "PatientID cannot be less than zero." });
+            }
+
+            if (User != null && User.ID != PatientID)
+            {
+                validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "PatientID dosent match." });
+            }
+
+            if (string.IsNullOrWhiteSpace(SSN) == true)
+            {
+                validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "SSN cannot be empty." });
+            }
+
+            //if (string.IsNullOrWhiteSpace(WCBNo) == true)
+            //{
+            //    validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "WCBNo cannot be empty." });
+            //}
+
+            //if (string.IsNullOrWhiteSpace(JobTitle) == true)
+            //{
+            //    validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "JobTitle cannot be empty." });
+            //}
+
+            //if (string.IsNullOrWhiteSpace(WorkActivities) == true)
+            //{
+            //    validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "WorkActivities cannot be empty." });
+            //}
+
+            //if (string.IsNullOrWhiteSpace(CarrierCaseNo) == true)
+            //{
+            //    validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "CarrierCaseNo cannot be empty." });
+            //}
+
+            if (string.IsNullOrWhiteSpace(ChartNo) == true)
+            {
+                validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "ChartNo cannot be empty." });
+            }
+
+            if (CompanyID < 0)
+            {
+                validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "CompanyID cannot be less than zero." });
+            }
+
+            if (Company != null && Company.ID != CompanyID)
+            {
+                validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "CompanyID dosent match." });
+            }
+
+            if (LocationID < 0)
+            {
+                validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "LocationID cannot be less than zero." });
+            }
+
+            if (Location != null && Location.ID != LocationID)
+            {
+                validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "LocationID dosent match." });
+            }
+
+            if (Weight < 0)
+            {
+                validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "Weight cannot be less than zero." });
+            }
+
+            if (MaritalStatus < 0)
+            {
+                validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "Please select MaritalStatus." });
+            }
+
+            //if (string.IsNullOrWhiteSpace(DrivingLicence) == true)
+            //{
+            //    validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "DrivingLicence cannot be empty." });
+            //}
+
+            //if (string.IsNullOrWhiteSpace(EmergenceyContact) == true)
+            //{
+            //    validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "EmergenceyContact cannot be empty." });
+            //}
+
+            //if (string.IsNullOrWhiteSpace(EmergenceyContactNumber) == true)
+            //{
+            //    validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "EmergenceyContactNumber cannot be empty." });
+            //}
+
+            //if (string.IsNullOrWhiteSpace(EmergenceyContactRelation) == true)
+            //{
+            //    validations.Add(new BusinessValidation { ValidationResult = BusinessValidationResult.Failure, ValidationMessage = "EmergenceyContactRelation cannot be empty." });
+            //}
+
             return validations;
         }
     }
