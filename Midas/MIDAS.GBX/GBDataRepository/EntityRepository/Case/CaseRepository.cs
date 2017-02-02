656af13dcb591f35bd859b7fb45aa7a547995cd1
milindkate21 <mkate@greenyourbills.com>
2017-02-02 17:17:42 +0530

    Code for CaseController and CaseRepository

---
+++ Midas/MIDAS.GBX/GBDataRepository/EntityRepository/Case/CaseRepository.cs
@@ -0,0 +1,28 @@
+﻿using MIDAS.GBX.EntityRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MIDAS.GBX.DataRepository.Model;
+using System.Data.Entity;
+using BO = MIDAS.GBX.BusinessObjects;
+
+namespace MIDAS.GBX.DataRepository.EntityRepository.Common
+{
+    internal class CaseRepository : BaseEntityRepo, IDisposable
+    {
+        private DbSet<Case> _dbCase;
+
+        public CaseRepository(MIDASGBXEntities context) : base(context)
+        {
+            _dbCase = context.Set<Case>();
+        }
+
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Midas/MIDAS.GBX/MIDAS.GBX.WebAPI/Controllers/CaseController.cs
@@ -0,0 +1,48 @@
+﻿using MIDAS.GBX.BusinessObjects;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace MIDAS.GBX.WebAPI.Controllers
+{
+    [RoutePrefix("midasapi/Case")]
+
+    public class CaseController : ApiController
+    {
+        private IRequestHandler<Case> requestHandler;
+
+        public CaseController()
+        {
+            requestHandler = new GbApiRequestHandler<Case>();
+        }
+
+        // GET: api/Organizations/5
+        [HttpGet]
+        [Route("Get/{id}")]
+
+        public HttpResponseMessage Get(int id)
+        {
+            return requestHandler.GetObject(Request, id);
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+
+        public HttpResponseMessage Get([FromBody]Case data)
+        {
+            return requestHandler.GetGbObjects(Request, data);
+        }
+
+       
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+    }
+}
