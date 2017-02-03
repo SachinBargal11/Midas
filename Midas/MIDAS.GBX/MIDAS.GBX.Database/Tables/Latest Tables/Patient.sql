c3184b662ca297ef47222ce2db9c6e2a8b81842a
satish.k <satish.k@codearray.tech>
2017-02-03 15:07:47 +0530

    Modified Patient2 Table

--- Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/Patient.sql
+++ Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/Patient.sql
@@ -1,29 +1,39 @@
-﻿/*
-Table Name: [dbo].[Patient]
-[PatientID] not required since the id from user table can be used
-[DateOfBirth] not required since the user table has date of birth
-Address not included as User table has address info
-*/
+﻿
 CREATE TABLE [dbo].[Patient](
-	[id] [int] NOT NULL,
-	[SSN] [nvarchar](20) NOT NULL, /*Social Security Number*/
-	[WCBNo] [nvarchar](20) NULL, /*Since this info remains constant for the user in a lifetime*/
-	[LocationID] [int] NOT NULL, /*Location id where current case is registered*/
-	[Weight] [DECIMAL](5, 2) NOT NULL, /*Weight in KG OR Pounds*/
-	[MaritalStatus] [TINYINT] NULL, /*Current Status*/
-	[DrivingLicence] [NVARCHAR](15) NULL, /*Current Driving Licence Number*/
-	[EmergencyContactName] [NVARCHAR](50) NULL, /*Emergency Contact Person Name*/
-	[EmergencyContactRelation] [NVARCHAR](50) NULL, /*Emergency Contact Person's Relation*/
-	[EmergencyContactNumber] [NVARCHAR](50) NULL, /*Emergency Contact Person's Contact Number*/
-
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[PatientID] [int] NOT NULL,
+	[SSN] [nvarchar](50) NOT NULL,
+	[WCBNo] [nvarchar](50) NULL,
+	[JobTitle] [nvarchar](50) NULL,
+	[WorkActivities] [nvarchar](50) NULL,
+	[CarrierCaseNo] [nvarchar](50) NULL,
+	[ChartNo] [nvarchar](50) NOT NULL,
+	[CompanyID] [int] NOT NULL,
+	[LocationID] [int] NOT NULL,
 	[IsDeleted] [bit] NULL,
 	[CreateByUserID] [int] NOT NULL,
 	[CreateDate] [datetime2](7) NOT NULL,
 	[UpdateByUserID] [int] NULL,
-	[UpdateDate] [datetime2](7) NULL, 
-    CONSTRAINT [PK_Patient] PRIMARY KEY ([id])
+	[UpdateDate] [datetime2](7) NULL,
+	[Weight] [decimal](3, 2) NULL,
+	[MaritalStatus] [tinyint] NULL,
+	[DrivingLicence] [nvarchar](15) NULL,
+	[EmergenceyContact] [nvarchar](50) NULL,
+	[EmergenceyContactNumber] [nvarchar](50) NULL,
+	[EmergenceyContactRelation] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Patient] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
+GO
+
+ALTER TABLE [dbo].[Patient]  WITH CHECK ADD  CONSTRAINT [FK_Patient_Company] FOREIGN KEY([CompanyID])
+REFERENCES [dbo].[Company] ([id])
+GO
+
+ALTER TABLE [dbo].[Patient] CHECK CONSTRAINT [FK_Patient_Company]
 GO
 
 ALTER TABLE [dbo].[Patient]  WITH CHECK ADD  CONSTRAINT [FK_Patient_Location] FOREIGN KEY([LocationID])
@@ -33,16 +43,18 @@ GO
 ALTER TABLE [dbo].[Patient] CHECK CONSTRAINT [FK_Patient_Location]
 GO
 
-ALTER TABLE [dbo].[Patient]  WITH CHECK ADD  CONSTRAINT [FK_Patient_User] FOREIGN KEY([id])
+ALTER TABLE [dbo].[Patient]  WITH CHECK ADD  CONSTRAINT [FK_Patient_MaritalStatus] FOREIGN KEY([MaritalStatus])
+REFERENCES [dbo].[MaritalStatus] ([Id])
+GO
+
+ALTER TABLE [dbo].[Patient] CHECK CONSTRAINT [FK_Patient_MaritalStatus]
+GO
+
+ALTER TABLE [dbo].[Patient]  WITH CHECK ADD  CONSTRAINT [FK_Patient_User] FOREIGN KEY([PatientID])
 REFERENCES [dbo].[User] ([id])
 GO
 
 ALTER TABLE [dbo].[Patient] CHECK CONSTRAINT [FK_Patient_User]
 GO
 
-ALTER TABLE [dbo].[Patient]  WITH CHECK ADD  CONSTRAINT [FK_Patient_MaritalStatus] FOREIGN KEY([MaritalStatus])
-REFERENCES [dbo].[MaritalStatus] ([id])
-GO
 
-ALTER TABLE [dbo].[Patient] CHECK CONSTRAINT [FK_Patient_MaritalStatus]
-GO
