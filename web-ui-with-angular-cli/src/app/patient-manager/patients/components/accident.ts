2e240068839e2b3dcab293d140f52f0e25a03c1c
Mitali Patel <mpatel@greenyourbills.com>
2017-02-13 22:45:36 +0400

    fixed accident bug

--- web-ui-with-angular-cli/src/app/patient-manager/patients/components/accident.ts
+++ web-ui-with-angular-cli/src/app/patient-manager/patients/components/accident.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit, ElementRef} from '@angular/core';
-import {Validators,FormGroup, FormBuilder} from '@angular/forms';
-import {Router, ActivatedRoute} from '@angular/router';
-import {SessionStore} from '../../../commons/stores/session-store';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { SessionStore } from '../../../commons/stores/session-store';
 import { Accident } from '../models/accident';
 import { ErrorMessageFormatter } from '../../../commons/utils/ErrorMessageFormatter';
 import { NotificationsStore } from '../../../commons/stores/notifications-store';
@@ -14,6 +14,7 @@ import * as moment from 'moment';
 import { Address } from '../../../commons/models/address';
 import { AccidentStore } from '../stores/accident-store';
 import { PatientsStore } from '../stores/patients-store';
+import * as _ from 'underscore';
 
 @Component({
     selector: 'accident',
@@ -23,24 +24,15 @@ import { PatientsStore } from '../stores/patients-store';
 export class AccidentInfoComponent implements OnInit {
     states: any[];
     dateOfAdmission: Date;
-    accidentDate:Date;
+    accidentDate: Date;
     maxDate: Date;
     cities: any[];
-    accident: Accident[];
-    // currentAccidentJS:any = null;
-    currentAccident:Accident;
-    // accidentAddress:Address = new Address({});
-    // hospitalAddress:Address = new Address({});
-    accidentCities:any[];
+    accidents: Accident[];
+    currentAccident: Accident;
+    accidentCities: any[];
     patientId: number;
     selectedCity = 0;
     isCitiesLoading = false;
-    options = {
-        timeOut: 3000,
-        showProgressBar: true,
-        pauseOnHover: false,
-        clickToClose: false
-    };
     accidentform: FormGroup;
     accidentformControls;
     isSaveProgress = false;
@@ -49,7 +41,7 @@ export class AccidentInfoComponent implements OnInit {
     constructor(
         private fb: FormBuilder,
         private _router: Router,
-        public  _route: ActivatedRoute,
+        public _route: ActivatedRoute,
         private _statesStore: StatesStore,
         private _accidentStore: AccidentStore,
         private _notificationsStore: NotificationsStore,
@@ -58,74 +50,67 @@ export class AccidentInfoComponent implements OnInit {
         private _elRef: ElementRef,
         private _notificationsService: NotificationsService,
     ) {
-        this._route.parent.params.subscribe((routeParams:any) =>{
-            this.patientId = parseInt(routeParams.patientId);
+        this._route.parent.params.subscribe((routeParams: any) => {
+            this.patientId = parseInt(routeParams.patientId, 10);
             this._progressBarService.show();
             let result = this._accidentStore.getAccidents(this.patientId);
             result.subscribe(
-                (accident:Accident[]) => {
-                    this.accident = accident;
-                   
-                    
-                      for (var i = 0; i < this.accident.length; i++) {
+                (accidents: Accident[]) => {
+                    this.accidents = accidents;
+                    this.currentAccident = _.find(this.accidents, (accident) => {
+                        return accident.isCurrentAccident;
+                    });
+                    if (this.currentAccident) {
+                        this.dateOfAdmission = this.currentAccident.dateOfAdmission
+                            ? this.currentAccident.dateOfAdmission.toDate()
+                            : null;
+                        this.accidentDate = this.currentAccident.accidentDate
+                            ? this.currentAccident.accidentDate.toDate()
+                            : null;
 
-                         var summaryData = this.accident[i];
-                        
-                         if (summaryData.isCurrentAccident) {
-                            this.currentAccident = summaryData;
-                            this.dateOfAdmission = this.currentAccident.dateOfAdmission
-                                 ? this.currentAccident.dateOfAdmission.toDate()
-                                 : null;
-                            this.accidentDate = this.currentAccident.accidentDate
-                                ? this.currentAccident.accidentDate.toDate()
-                                : null;
-                           
-                            if (this.currentAccident.accidentAddress.state || this.currentAccident.hospitalAddress.state ){
+                        if (this.currentAccident.accidentAddress.state || this.currentAccident.hospitalAddress.state) {
                             this.loadCities(this.currentAccident.hospitalAddress.state);
                             this.loadAccidentCities(this.currentAccident.accidentAddress.state);
-                        
-                         }
-                         break;
-                        }else{
-
                         }
-                        
-                         
-                     }
-                    
+                    } else {
+                        this.currentAccident = new Accident({
+                            accidentAddress: new Address({}),
+                            hospitalAddress: new Address({})
+                        });
+                    }
                 },
-                 (error) => {
+                (error) => {
                     this._router.navigate(['../../']);
                     this._progressBarService.hide();
                 },
                 () => {
                     this._progressBarService.hide();
                 });
-    });
+        });
 
-      
+
         this.accidentform = this.fb.group({
-                doa: ['', Validators.required],
-                dot: ['', Validators.required],
-                plateNumber:['', Validators.required],
-                address: [''],
-                accidentAddress: [''],
-                accidentAddress2: [''],
-                address2: [''],
-                reportNumber:['', Validators.required],
-                hospitalName: ['', Validators.required],
-                describeInjury: ['', Validators.required],
-                patientType:['', Validators.required],
-                additionalPatient:[''],
-                state: [''],
-                city: [''],
-                zipcode: [''],
-                country: [''],
-                accidentState: [''],
-                accidentCity: [''],
-                accidentZipcode: [''],
-                accidentCountry: ['']
-            });
+            doa: ['', Validators.required],
+            dot: ['', Validators.required],
+            plateNumber: ['', Validators.required],
+            address: [''],
+            accidentAddress: [''],
+            accidentAddress2: [''],
+            address2: [''],
+            reportNumber: ['', Validators.required],
+            hospitalName: ['', Validators.required],
+            describeInjury: ['', Validators.required],
+            patientType: ['', Validators.required],
+            additionalPatient: [''],
+            state: [''],
+            city: [''],
+            zipcode: [''],
+            country: [''],
+            accidentState: [''],
+            accidentCity: [''],
+            accidentZipcode: [''],
+            accidentCountry: ['']
+        });
         this.accidentformControls = this.accidentform.controls;
     }
 
@@ -157,7 +142,7 @@ export class AccidentInfoComponent implements OnInit {
         }
     }
 
-      selectAccidentState(event) {
+    selectAccidentState(event) {
         this.selectedCity = 0;
         let currentState = event.target.value;
         this.loadAccidentCities(currentState);
@@ -182,96 +167,96 @@ export class AccidentInfoComponent implements OnInit {
         let addResult;
         let result;
         let accident = new Accident({
-              patientId: this.patientId,
-              isCurrentAccident:1,
-              plateNumber: accidentformValues.plateNumber,
-              reportNumber: accidentformValues.reportNumber,
-              hospitalName:accidentformValues.hospitalName,
-              describeInjury:accidentformValues.describeInjury,
-              dateOfAdmission:accidentformValues.dot ? moment(accidentformValues.dot):null,
-              patientTypeId:parseInt(accidentformValues.patientType),
-              additionalPatients:accidentformValues.additionalPatient,
-              accidentDate:accidentformValues.doa ? moment(accidentformValues.doa):null,
-              accidentAddress: new Address({
-                    address1:accidentformValues.accidentAddress,
-                    address2:accidentformValues.accidentAddress2,
-                    city:accidentformValues.accidentCity,
-                    country:accidentformValues.accidentCountry,
-                    state: accidentformValues.accidentState,
-                    zipCode:accidentformValues.accidentZipcode  
-                }),
-                hospitalAddress: new Address({
-                address1:accidentformValues.address,
-                address2:accidentformValues.address2,
-                city:accidentformValues.city,
-                country:accidentformValues.country,
-                state:accidentformValues.state,
-                zipCode:accidentformValues.zipcode
+            patientId: this.patientId,
+            isCurrentAccident: 1,
+            plateNumber: accidentformValues.plateNumber,
+            reportNumber: accidentformValues.reportNumber,
+            hospitalName: accidentformValues.hospitalName,
+            describeInjury: accidentformValues.describeInjury,
+            dateOfAdmission: accidentformValues.dot ? moment(accidentformValues.dot) : null,
+            patientTypeId: parseInt(accidentformValues.patientType),
+            additionalPatients: accidentformValues.additionalPatient,
+            accidentDate: accidentformValues.doa ? moment(accidentformValues.doa) : null,
+            accidentAddress: new Address({
+                address1: accidentformValues.accidentAddress,
+                address2: accidentformValues.accidentAddress2,
+                city: accidentformValues.accidentCity,
+                country: accidentformValues.accidentCountry,
+                state: accidentformValues.accidentState,
+                zipCode: accidentformValues.accidentZipcode
+            }),
+            hospitalAddress: new Address({
+                address1: accidentformValues.address,
+                address2: accidentformValues.address2,
+                city: accidentformValues.city,
+                country: accidentformValues.country,
+                state: accidentformValues.state,
+                zipCode: accidentformValues.zipcode
             })
         });
         this._progressBarService.show();
         //
-        if(this.currentAccident.id){
-              result = this._accidentStore.updateAccident(accident,this.currentAccident.id);
-        result.subscribe(
-            (response) => {
-                let notification = new Notification({
-                    'title': 'Accident Information updated successfully!',
-                    'type': 'SUCCESS',
-                    'createdAt': moment()
+        if (this.currentAccident.id) {
+            result = this._accidentStore.updateAccident(accident, this.currentAccident.id);
+            result.subscribe(
+                (response) => {
+                    let notification = new Notification({
+                        'title': 'Accident Information updated successfully!',
+                        'type': 'SUCCESS',
+                        'createdAt': moment()
+                    });
+                    this._notificationsStore.addNotification(notification);
+                    this._router.navigate(['/patient-manager/patients']);
+                },
+                (error) => {
+                    let errString = 'Unable to update accident information.';
+                    let notification = new Notification({
+                        'messages': ErrorMessageFormatter.getErrorMessages(error, errString),
+                        'type': 'ERROR',
+                        'createdAt': moment()
+                    });
+                    this.isSaveAccidentProgress = false;
+                    this._notificationsStore.addNotification(notification);
+                    this._notificationsService.error('Oh No!', ErrorMessageFormatter.getErrorMessages(error, errString));
+                    this._progressBarService.hide();
+                },
+                () => {
+                    this.isSaveAccidentProgress = false;
+                    this._progressBarService.hide();
                 });
-                this._notificationsStore.addNotification(notification);
-                this._router.navigate(['/patient-manager/patients']);
-            },
-            (error) => {
-                let errString = 'Unable to update accident information.';
-                let notification = new Notification({
-                    'messages': ErrorMessageFormatter.getErrorMessages(error, errString),
-                    'type': 'ERROR',
-                    'createdAt': moment()
-                });
-                this.isSaveAccidentProgress = false;
-                this._notificationsStore.addNotification(notification);
-                this._notificationsService.error('Oh No!', ErrorMessageFormatter.getErrorMessages(error, errString));
-                this._progressBarService.hide();
-            },
-            () => {
-                this.isSaveAccidentProgress = false;
-                this._progressBarService.hide();
-            });
 
         }
         //
-         else{
-        addResult = this._accidentStore.addAccident(accident);
-        
-        addResult.subscribe(
-            (response) => {
-                let notification = new Notification({
-                    'title': 'Accident Information added successfully!',
-                    'type': 'SUCCESS',
-                    'createdAt': moment()
+        else {
+            addResult = this._accidentStore.addAccident(accident);
+
+            addResult.subscribe(
+                (response) => {
+                    let notification = new Notification({
+                        'title': 'Accident Information added successfully!',
+                        'type': 'SUCCESS',
+                        'createdAt': moment()
+                    });
+                    this._notificationsStore.addNotification(notification);
+                    this._router.navigate(['/patient-manager/patients']);
+                },
+                (error) => {
+                    let errString = 'Unable to add accident information.';
+                    let notification = new Notification({
+                        'messages': ErrorMessageFormatter.getErrorMessages(error, errString),
+                        'type': 'ERROR',
+                        'createdAt': moment()
+                    });
+                    this.isSaveAccidentProgress = false;
+                    this._notificationsStore.addNotification(notification);
+                    this._notificationsService.error('Oh No!', ErrorMessageFormatter.getErrorMessages(error, errString));
+                    this._progressBarService.hide();
+                },
+                () => {
+                    this.isSaveAccidentProgress = false;
+                    this._progressBarService.hide();
                 });
-                this._notificationsStore.addNotification(notification);
-                this._router.navigate(['/patient-manager/patients']);
-            },
-            (error) => {
-                let errString = 'Unable to add accident information.';
-                let notification = new Notification({
-                    'messages': ErrorMessageFormatter.getErrorMessages(error, errString),
-                    'type': 'ERROR',
-                    'createdAt': moment()
-                });
-                this.isSaveAccidentProgress = false;
-                this._notificationsStore.addNotification(notification);
-                this._notificationsService.error('Oh No!', ErrorMessageFormatter.getErrorMessages(error, errString));
-                this._progressBarService.hide();
-            },
-            () => {
-                this.isSaveAccidentProgress = false;
-                this._progressBarService.hide();
-            });
-         }
+        }
     }
 
 }
