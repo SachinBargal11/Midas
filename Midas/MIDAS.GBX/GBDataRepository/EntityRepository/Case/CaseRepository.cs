6bdf8aa3d54d07724e24dfa3b8934000ebceb657
milindkate21 <mkate@greenyourbills.com>
2017-02-14 11:02:55 +0530

    Code added for CaseAPI

--- Midas/MIDAS.GBX/GBDataRepository/EntityRepository/Case/CaseRepository.cs
+++ Midas/MIDAS.GBX/GBDataRepository/EntityRepository/Case/CaseRepository.cs
@@ -20,56 +20,8 @@ namespace MIDAS.GBX.DataRepository.EntityRepository.Common
             context.Configuration.ProxyCreationEnabled = false;
         }
 
-        /*
         #region Entity Conversion
         public override T Convert<T, U>(U entity)
-        {
-            List<Case> listcase = entity as List<Case>;
-            if (listcase == null)
-                return default(T);
-
-            List<BO.Case> BOcaselist = new List<BO.Case>();
-            foreach (var mcase in listcase)
-            {
-                BO.Case boCase = new BO.Case();
-
-                boCase.ID = mcase.Id;
-                boCase.PatientId = mcase.PatientId;
-                boCase.CaseName = mcase.CaseName;
-                boCase.CaseTypeId = (int)mcase.CaseTypeId;
-                boCase.Age = (decimal)mcase.Age;
-                boCase.DateOfInjury = mcase.DateOfInjury;
-                boCase.LocationId = mcase.LocationId;
-                boCase.PatientAddressId = mcase.PatientAddressId;
-                boCase.PatientContactInfoId =mcase.PatientContactInfoId;
-                boCase.EmpInfo =mcase.EmpInfo;
-                boCase.InsuranceInfoId =mcase.InsuranceInfoId;
-                boCase.VehiclePlateNo = mcase.VehiclePlateNo;
-                boCase.AccidentAddressId = mcase.AccidentAddressId;
-                boCase.CarrierCaseNo = mcase.CarrierCaseNo;
-                boCase.Transportation = mcase.Transportation;
-                boCase.DateOfFirstTreatment = mcase.DateOfFirstTreatment;
-                boCase.CaseStatusId = mcase.CaseStatusId;
-                boCase.AttorneyId = mcase.AttorneyId;
-
-                //BO.User boUser = new BO.User();
-                //using (UserRepository cmp = new UserRepository(_context))
-                //{
-                //    boUser = cmp.Convert<BO.User, User>(mcase.User);
-                //    boCase.User = boUser;
-                //}
-
-                BOcaselist.Add(boCase);
-            }
-
-
-            return (T)(object)BOcaselist;
-        }
-        #endregion
-        */
-        /*
-        #region Entity Conversion
-        public override T ObjectConvert<T, U>(U entity)
         {
             Case cases = entity as Case;
 
@@ -77,41 +29,45 @@ namespace MIDAS.GBX.DataRepository.EntityRepository.Common
                 return default(T);
 
             BO.Case caseBO = new BO.Case();
+
             caseBO.ID = cases.Id;
-            caseBO.PatientId = cases.PatientId;
             caseBO.CaseName = cases.CaseName;
-            caseBO.CaseTypeId = (int)cases.CaseTypeId;
-            caseBO.Age = (decimal)cases.Age;
+            caseBO.CaseTypeId = cases.CaseTypeId;
             caseBO.DateOfInjury = cases.DateOfInjury;
             caseBO.LocationId = cases.LocationId;
-            caseBO.PatientAddressId = cases.PatientAddressId;
-            caseBO.PatientContactInfoId = cases.PatientContactInfoId;
-            caseBO.EmpInfo = cases.EmpInfo;
-            caseBO.InsuranceInfoId = cases.InsuranceInfoId;
-            caseBO.VehiclePlateNo = cases.VehiclePlateNo;
-            caseBO.AccidentAddressId = cases.AccidentAddressId;
+            caseBO.PatientEmpInfoId = cases.PatientEmpInfoId;
+            caseBO.PatientInsuranceInfoId = cases.PatientInsuranceInfoId;
+            caseBO.PatientAccidentInfoId = cases.PatientAccidentInfoId;
+            caseBO.RefferingOfficeId = cases.RefferingOfficeId;
             caseBO.CarrierCaseNo = cases.CarrierCaseNo;
             caseBO.Transportation = cases.Transportation;
-            caseBO.DateOfFirstTreatment = cases.DateOfFirstTreatment;
             caseBO.CaseStatusId = cases.CaseStatusId;
             caseBO.AttorneyId = cases.AttorneyId;
 
-            
-
-
-
+            if (cases.IsDeleted.HasValue)
+                caseBO.IsDeleted = cases.IsDeleted.Value;
+            if (cases.UpdateByUserID.HasValue)
+                caseBO.UpdateByUserID = cases.UpdateByUserID.Value;
 
 
             return (T)(object)caseBO;
         }
         #endregion
-        */
+
+        #region Validate Entities
+        public override List<MIDAS.GBX.BusinessObjects.BusinessValidation> Validate<T>(T entity)
+        {
+            BO.Case cases = (BO.Case)(object)entity;
+            var result = cases.Validate(cases);
+            return result;
+        }
+        #endregion
 
         #region Get By ID
         public override object Get(int id)
         {
-            var acc = _context.Cases.Include("User").Include("Location").Where(p => p.Id == id ).FirstOrDefault<Case>();
-            BO.Case acc_ = ObjectConvert<BO.Case, Case>(acc);
+            var acc = _context.Cases.Include("Patient2").Include("PatientAccidentInfo").Include("PatientEmpInfo").Include("PatientInsuranceInfo").Include("RefferingOffice").Where(p => p.Id == id).FirstOrDefault<Case>();
+            BO.Case acc_ = Convert<BO.Case, Case>(acc);
 
             if (acc_ == null)
             {
@@ -122,26 +78,106 @@ namespace MIDAS.GBX.DataRepository.EntityRepository.Common
         }
         #endregion
 
-        #region Get All 
-        public override Object Get<T>(T entity)
+        #region Get By Patient Id
+        public override object GetByPatientId(int PatientId)
         {
-            var acc = _context.Cases.Include("User").Include("Location").ToList<Case>();
+            var acc = _context.Cases.Include("Patient2").Include("PatientAccidentInfo").Include("PatientEmpInfo").Include("PatientInsuranceInfo").Include("RefferingOffice").Where(p => p.PatientId == PatientId && (p.IsDeleted.HasValue == false || p.IsDeleted == false)).ToList<Case>();
+
             if (acc == null)
             {
                 return new BO.ErrorObject { ErrorMessage = "No record found.", errorObject = "", ErrorLevel = ErrorLevel.Error };
             }
-            else
+
+            List<BO.Case> lstcase = new List<BO.Case>();
+            foreach (Case item in acc)
             {
-                List<BO.Case> acc_ = Convert<List<BO.Case>, List<Case>>(acc);
-                return (object)acc_;
+                lstcase.Add(Convert<BO.Case, Case>(item));
             }
 
+            return lstcase;
+        }
+        #endregion 
+
+
+        #region save
+        public override object Save<T>(T entity)
+        {
+            BO.Case caseBO = (BO.Case)(object)entity;
+            BO.Patient2 patient2BO = new BO.Patient2();
+            BO.Location locationBO = new BO.Location();
+            BO.PatientAccidentInfo patientAccidentInfoBO = new BO.PatientAccidentInfo();
+            BO.PatientEmpInfo patientEmpInfoBO = new BO.PatientEmpInfo();
+            BO.PatientInsuranceInfo patientInsuranceInfo = new BO.PatientInsuranceInfo();
+            BO.RefferingOffice refferingOffice = new BO.RefferingOffice();
+
+            Case caseDB = new Case();
+
+            using (var dbContextTransaction = _context.Database.BeginTransaction())
+            {
+                Patient2 patient2DB = new Patient2();
+                Location locationDB = new Location();
+
+                bool IsEditMode = false;
+                IsEditMode = (caseBO != null && caseBO.ID > 0) ? true : false;
+
+
+                #region case
+                if (caseBO != null)
+                {
+                    bool Add_caseDB = false;
+                    caseDB = _context.Cases.Where(p => p.Id == caseBO.ID).FirstOrDefault();
+
+                    if (caseDB == null && caseBO.ID <= 0)
+                    {
+                        caseDB = new Case();
+                        Add_caseDB = true;
+                    }
+                    else if (caseDB == null && caseBO.ID > 0)
+                    {
+                        dbContextTransaction.Rollback();
+                        return new BO.ErrorObject { errorObject = "", ErrorMessage = "Case dosent exists.", ErrorLevel = ErrorLevel.Error };
+                    }
+
+                    caseDB.PatientId = caseBO.PatientId;
+                    caseDB.CaseName = IsEditMode == true && caseBO.CaseName == null ? caseDB.CaseName : caseBO.CaseName;
+                    caseDB.CaseTypeId = IsEditMode == true && caseBO.CaseTypeId == null ? caseDB.CaseTypeId : caseBO.CaseTypeId;
+                    caseDB.DateOfInjury = IsEditMode == true && caseBO.DateOfInjury == null ? caseDB.DateOfInjury : caseBO.DateOfInjury;
+                    caseDB.LocationId = IsEditMode == true && caseBO.LocationId.HasValue==false ? caseDB.LocationId : caseBO.LocationId.Value;
+                    caseDB.PatientEmpInfoId = IsEditMode == true && caseBO.PatientEmpInfoId.HasValue == false ? caseDB.PatientEmpInfoId : caseBO.PatientEmpInfoId.Value;
+                    caseDB.PatientInsuranceInfoId = IsEditMode == true && caseBO.PatientInsuranceInfoId.HasValue == false ? caseDB.PatientInsuranceInfoId : caseBO.PatientInsuranceInfoId.Value;
+                    caseDB.PatientAccidentInfoId = IsEditMode == true && caseBO.PatientAccidentInfoId.HasValue == false ? caseDB.PatientAccidentInfoId : caseBO.PatientAccidentInfoId.Value;
+                    caseDB.RefferingOfficeId = IsEditMode == true && caseBO.RefferingOfficeId.HasValue == false ? caseDB.RefferingOfficeId : caseBO.RefferingOfficeId.Value;
+                    caseDB.CarrierCaseNo = IsEditMode == true && caseBO.CarrierCaseNo == null ? caseDB.CarrierCaseNo : caseBO.CarrierCaseNo;
+                    caseDB.Transportation = IsEditMode == true && caseBO.Transportation.HasValue == false ? caseDB.Transportation : caseBO.Transportation.Value;
+                    caseDB.CaseStatusId = IsEditMode == true && caseBO.CaseStatusId.HasValue == false ? caseDB.CaseStatusId : caseBO.CaseStatusId.Value;
+                    caseDB.AttorneyId = IsEditMode == true && caseBO.AttorneyId.HasValue == false ? caseDB.AttorneyId : caseBO.AttorneyId.Value;
+                    if (Add_caseDB == true)
+                    {
+                        caseDB = _context.Cases.Add(caseDB);
+                    }
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    if (IsEditMode == false)
+                    {
+                        dbContextTransaction.Rollback();
+                        return new BO.ErrorObject { errorObject = "", ErrorMessage = "Please pass valid case details.", ErrorLevel = ErrorLevel.Error };
+                    }
+                    caseDB = null;
+                }
+
+                _context.SaveChanges();
+                #endregion
+                dbContextTransaction.Commit();
+
+                caseDB = _context.Cases.Where(p => p.Id == caseDB.Id).FirstOrDefault<Case>();
+            }
+
+            var res = Convert<BO.Case, Case>(caseDB);
+            return (object)res;
         }
         #endregion
-
-      
-
-  
 
 
         public void Dispose()
