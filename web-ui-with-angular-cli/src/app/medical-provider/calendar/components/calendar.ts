ef2bbb8d7465873bec1267efec28568b48d1fcd5
Mitali Patel <mpatel@greenyourbills.com>
2017-02-26 18:44:58 +0400

    added fields for recurrence

--- web-ui-with-angular-cli/src/app/medical-provider/calendar/components/calendar.ts
+++ web-ui-with-angular-cli/src/app/medical-provider/calendar/components/calendar.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 import { ScheduledEvent } from '../../../commons/models/scheduled-event';
 import { ScheduledEventInstance } from '../../../commons/models/scheduled-event-instance';
 import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core';
@@ -27,15 +28,31 @@ export class CalendarComponent implements OnInit {
     dialogVisible: boolean = false;
     selectedEvent: ScheduledEvent;
     startDate: Date;
+    calendarForm: FormGroup;
+    calendarFormControls;
+    private scheduledEventEditorValid: boolean = false;
 
     constructor(
         private _router: Router,
+        private _fb: FormBuilder,
         private _cd: ChangeDetectorRef,
         private _sessionStore: SessionStore,
         private _scheduledEventStore: ScheduledEventStore,
         private _notificationsStore: NotificationsStore,
         private _progressBarService: ProgressBarService
     ) {
+        this.calendarForm = this._fb.group({
+
+        });
+        this.calendarFormControls = this.calendarForm.controls;
+    }
+
+    isFormValid() {
+        if (this.scheduledEventEditorValid && this.calendarForm.valid) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     ngOnInit() {
@@ -74,8 +91,18 @@ export class CalendarComponent implements OnInit {
             });
     }
 
-    handleDayClick(event) {
+    handleDialogHide() {
         this.selectedEvent = null;
+    }
+
+    handleDayClick(event) {
+        this.selectedEvent = new ScheduledEvent({
+            name: '',
+            eventStart: event.date,
+            eventEnd: event.date,
+            timezone: '',
+            isAllDay: false
+        });
         this.dialogVisible = true;
         this._cd.detectChanges();
     }
@@ -90,5 +117,61 @@ export class CalendarComponent implements OnInit {
     saveEvent() {
         let updatedEvent: ScheduledEvent = this._scheduledEventEditorComponent.getEditedEvent();
         console.log(updatedEvent);
+        if (updatedEvent.id) {
+            let result = this._scheduledEventStore.updateEvent(updatedEvent);
+            result.subscribe(
+                (response) => {
+                    let notification = new Notification({
+                        'title': 'Event updated successfully!',
+                        'type': 'SUCCESS',
+                        'createdAt': moment()
+                    });
+                    this.loadEvents();
+                    this._notificationsStore.addNotification(notification);
+                },
+                (error) => {
+                    let errString = 'Unable to update event!';
+                    let notification = new Notification({
+                        'messages': ErrorMessageFormatter.getErrorMessages(error, errString),
+                        'type': 'ERROR',
+                        'createdAt': moment()
+                    });
+                    this._progressBarService.hide();
+                    this._notificationsStore.addNotification(notification);
+                },
+                () => {
+                    this._progressBarService.hide();
+                });
+        }
+        // new
+        else {
+            let result = this._scheduledEventStore.addEvent(updatedEvent);
+            result.subscribe(
+                (response) => {
+                    let notification = new Notification({
+                        'title': 'Event added successfully!',
+                        'type': 'SUCCESS',
+                        'createdAt': moment()
+                    });
+                    this.loadEvents();
+                    this._notificationsStore.addNotification(notification);
+                    // this.event = null;
+                },
+                (error) => {
+                    let errString = 'Unable to add event!';
+                    let notification = new Notification({
+                        'messages': ErrorMessageFormatter.getErrorMessages(error, errString),
+                        'type': 'ERROR',
+                        'createdAt': moment()
+                    });
+                    this._progressBarService.hide();
+                    this._notificationsStore.addNotification(notification);
+                },
+                () => {
+                    this._progressBarService.hide();
+                });
+        }
+
+        this.dialogVisible = false;
     }
 }
--- web-ui-with-angular-cli/src/app/medical-provider/calendar/components/scheduled-event-editor.ts
+++ web-ui-with-angular-cli/src/app/medical-provider/calendar/components/scheduled-event-editor.ts
@@ -1,5 +1,6 @@
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 import { ScheduledEvent } from '../../../commons/models/scheduled-event';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import * as moment from 'moment';
 import * as _ from 'underscore';
 
@@ -9,21 +10,40 @@ import * as _ from 'underscore';
 })
 
 
-export class ScheduledEventEditorComponent implements OnInit {
+export class ScheduledEventEditorComponent implements OnChanges {
 
     private _selectedEvent: ScheduledEvent;
     eventStartAsDate: Date;
     eventEndAsDate: Date;
+    isAllDay: boolean;
+    repeatType: number = 0;
+    repeatEvery: number = 0;
+    end: string = '0';
+    repeatOnWeekDay: string[] = ['Mo'];
+    recur_count: number = 0;
+    recur_until: Date;
+    recur_month: number = 1;
+    recur_weekday: number = 1;
+    recur_monthday_radio: number = 0;
+    recur_monthday: number = 0;
+    recur_weekday_offset: number = 0;
+    recur_year_radio: number = 0;
+    scheduledEventEditorForm: FormGroup;
+    scheduledEventEditorFormControls;
+    @Output() isValid = new EventEmitter();
 
     @Input() set selectedEvent(value: ScheduledEvent) {
         if (value) {
             this._selectedEvent = value;
             this.eventStartAsDate = this._selectedEvent.eventStartAsDate;
             this.eventEndAsDate = this._selectedEvent.eventEndAsDate;
+            this.isAllDay = this._selectedEvent.isAllDay;
+            this.recur_until = this._selectedEvent.eventStartAsDate;
         } else {
             this._selectedEvent = null;
             this.eventStartAsDate = null;
             this.eventEndAsDate = null;
+            this.isAllDay = false;
         }
     }
 
@@ -31,18 +51,54 @@ export class ScheduledEventEditorComponent implements OnInit {
         return this._selectedEvent;
     }
 
-
     constructor(
+        private _fb: FormBuilder
     ) {
+        this.scheduledEventEditorForm = this._fb.group({
+            name: ['', Validators.required],
+            eventStartDate: ['', Validators.required],
+            eventStartTime: ['', Validators.required],
+            eventEndDate: ['', Validators.required],
+            eventEndTime: ['', Validators.required],
+            isAllDay: [],
+            repeatType: [],
+            repeatEvery: [],
+            end: [],
+            recur_count: [],
+            recur_until: [],
+            repeatOnWeekDay: [],
+            recur_monthday_radio: [],
+            recur_monthday: [],
+            recur_weekday_offset: [],
+            recur_weekday: [],
+            recur_year_radio: [],
+            recur_month: []
+        });
+        this.scheduledEventEditorFormControls = this.scheduledEventEditorForm.controls;
+        this.scheduledEventEditorForm.valueChanges.subscribe(() => {
+            if (this.scheduledEventEditorForm.valid) {
+                this.isValid.emit(true);
+            } else {
+                this.isValid.emit(false);
+            }
+        });
     }
 
-    ngOnInit() {
-
+    ngOnChanges() {
+        // if (!this.userProfile) {
+        //     this.userProfile = BlankUserProfile;
+        // }
     }
 
     getEditedEvent(): ScheduledEvent {
+        debugger;
+        let scheduledEventEditorFormValues = this.scheduledEventEditorForm.value;
         return new ScheduledEvent({
-            name: 'Updated Event'
+            id: this.selectedEvent.id,
+            name: scheduledEventEditorFormValues.name,
+            eventStart: scheduledEventEditorFormValues.isAllDay ? moment.utc(this.eventStartAsDate).startOf('day') : moment(this.eventStartAsDate),
+            eventEnd: scheduledEventEditorFormValues.isAllDay ? moment.utc(this.eventEndAsDate).endOf('day') : moment(this.eventEndAsDate),
+            isAllDay: scheduledEventEditorFormValues.isAllDay
         });
     }
 
