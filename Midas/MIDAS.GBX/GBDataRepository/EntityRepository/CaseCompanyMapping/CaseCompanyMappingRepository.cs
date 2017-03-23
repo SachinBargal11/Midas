fa0db637fc711b82e9ec535ab0e36a04e601e610
milindkate21 <mkate@greenyourbills.com>
2017-03-23 17:16:46 +0530

    Code added in CaseCompanyMappingAPI

--- Midas/MIDAS.GBX/GBDataRepository/EntityRepository/CaseCompanyMapping/CaseCompanyMappingRepository.cs
+++ Midas/MIDAS.GBX/GBDataRepository/EntityRepository/CaseCompanyMapping/CaseCompanyMappingRepository.cs
@@ -24,33 +24,15 @@ namespace MIDAS.GBX.DataRepository.EntityRepository.Common
         public override T Convert<T, U>(U entity)
         {
             CaseCompanyMapping caseCompanyMappings = entity as CaseCompanyMapping;
-
             if (caseCompanyMappings == null)
                 return default(T);
-
             BO.CaseCompanyMapping caseCompanyMappingBO = new BO.CaseCompanyMapping();
-
             caseCompanyMappingBO.ID = caseCompanyMappings.Id;
             caseCompanyMappingBO.CaseId = caseCompanyMappings.CaseId;
             caseCompanyMappingBO.CompanyId = caseCompanyMappings.CompanyId;
             caseCompanyMappingBO.IsDeleted = caseCompanyMappings.IsDeleted;
             caseCompanyMappingBO.CreateByUserID = caseCompanyMappings.CreateByUserID;
             caseCompanyMappingBO.UpdateByUserID = caseCompanyMappings.UpdateByUserID;
-
-            //BO.Case boCase = new BO.Case();
-            //using (CaseRepository cmp = new CaseRepository(_context))
-            //{
-
-            //    boCase = cmp.Convert<BO.Case, Case>(caseCompanyMappings.Case);
-            //    caseCompanyMappingBO.Case = boCase;
-            //}
-            //BO.Company boCompany = new BO.Company();
-            //using (CompanyRepository cmp = new CompanyRepository(_context))
-            //{
-
-            //    boCompany = cmp.Convert<BO.Company, Company>(caseCompanyMappings.Company);
-            //    caseCompanyMappingBO.Company = boCompany;
-            //}
 
             return (T)(object)caseCompanyMappingBO;
         }
@@ -62,6 +44,159 @@ namespace MIDAS.GBX.DataRepository.EntityRepository.Common
             BO.CaseCompanyMapping CaseCompanyMapping = (BO.CaseCompanyMapping)(object)entity;
             var result = CaseCompanyMapping.Validate(CaseCompanyMapping);
             return result;
+        }
+        #endregion
+
+        #region Save
+        public override object Save<T>(T entity)
+        {
+            BO.CaseCompanyMapping CaseCompanyMappingBO = (BO.CaseCompanyMapping)(object)entity;
+            List<BO.CaseCompanyMapping> lstCaseCompanyMapping = new List<BO.CaseCompanyMapping>();
+            CaseCompanyMapping caseCompanyMappingDB = new CaseCompanyMapping();
+
+            using (var dbContextTransaction = _context.Database.BeginTransaction())
+            {
+                bool IsEditMode = false;
+                IsEditMode = (CaseCompanyMappingBO != null && CaseCompanyMappingBO.ID > 0) ? true : false;
+
+                #region case
+                if (CaseCompanyMappingBO != null)
+                {
+                    bool Add_caseDB = false;
+                    caseCompanyMappingDB = _context.CaseCompanyMappings.Where(p => p.Id == CaseCompanyMappingBO.ID
+                                                                        && (p.IsDeleted.HasValue == false || (p.IsDeleted.HasValue == true && p.IsDeleted.Value == false)))
+                                                                        .FirstOrDefault();
+
+                    if (caseCompanyMappingDB == null && CaseCompanyMappingBO.ID <= 0)
+                    {
+                        caseCompanyMappingDB = new CaseCompanyMapping();
+                        Add_caseDB = true;
+                    }
+                    else if (caseCompanyMappingDB == null && CaseCompanyMappingBO.ID > 0)
+                    {
+                        dbContextTransaction.Rollback();
+                        return new BO.ErrorObject { errorObject = "", ErrorMessage = "Case dosent exists.", ErrorLevel = ErrorLevel.Error };
+                    }
+                    if (Add_caseDB == true)
+                    {
+                        if (_context.CaseCompanyMappings.Any(p => p.CaseId == CaseCompanyMappingBO.CaseId && p.CompanyId == CaseCompanyMappingBO.CompanyId
+                                                         && (p.IsDeleted.HasValue == false || (p.IsDeleted.HasValue == true && p.IsDeleted.Value == false))))
+                        {
+                            dbContextTransaction.Rollback();
+                            return new BO.ErrorObject { errorObject = "", ErrorMessage = "Case already exists.", ErrorLevel = ErrorLevel.Error };
+                        }
+                    }
+
+                    caseCompanyMappingDB.CaseId = CaseCompanyMappingBO.CaseId;
+                    caseCompanyMappingDB.CompanyId = CaseCompanyMappingBO.CompanyId;
+
+                    if (Add_caseDB == true)
+                    {
+                        caseCompanyMappingDB = _context.CaseCompanyMappings.Add(caseCompanyMappingDB);
+                    }
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    if (IsEditMode == false)
+                    {
+                        dbContextTransaction.Rollback();
+                        return new BO.ErrorObject { errorObject = "", ErrorMessage = "Please pass valid details.", ErrorLevel = ErrorLevel.Error };
+                    }
+                    caseCompanyMappingDB = null;
+                }
+
+                _context.SaveChanges();
+                #endregion
+
+                dbContextTransaction.Commit();
+                caseCompanyMappingDB = _context.CaseCompanyMappings.Where(p => p.Id == caseCompanyMappingDB.Id 
+                                                                    && (p.IsDeleted.HasValue == false || (p.IsDeleted.HasValue == true && p.IsDeleted.Value == false)))
+                                                                    .FirstOrDefault<CaseCompanyMapping>();
+            }
+            var res = Convert<BO.CaseCompanyMapping, CaseCompanyMapping>(caseCompanyMappingDB);
+            return (object)res;
+        }
+        #endregion
+
+        #region Get By ID
+        public override object Get(int id)
+        {
+            var acc = _context.CaseCompanyMappings.Where(p => p.Id == id
+                                                  && (p.IsDeleted.HasValue == false || (p.IsDeleted.HasValue == true && p.IsDeleted.Value == false)))
+                                                  .FirstOrDefault<CaseCompanyMapping>();
+
+            BO.CaseCompanyMapping acc_ = Convert<BO.CaseCompanyMapping, CaseCompanyMapping>(acc);
+            if (acc_ == null)
+            {
+                return new BO.ErrorObject { ErrorMessage = "No record found.", errorObject = "", ErrorLevel = ErrorLevel.Error };
+            }
+            return (object)acc_;
+        }
+        #endregion
+
+        #region Get By Case Id
+        public override object GetByCaseId(int CaseId)
+        {
+            var acc = _context.CaseCompanyMappings.Where(p => p.CaseId == CaseId
+                                                   && (p.IsDeleted.HasValue == false || (p.IsDeleted.HasValue == true && p.IsDeleted.Value == false)))
+                                                   .ToList<CaseCompanyMapping>();
+            if (acc == null)
+            {
+                return new BO.ErrorObject { ErrorMessage = "No record found for this Case Id.", errorObject = "", ErrorLevel = ErrorLevel.Error };
+            }
+            else
+            {
+                List<BO.CaseCompanyMapping> lstCaseCompanyMapping = new List<BO.CaseCompanyMapping>();
+                foreach (CaseCompanyMapping item in acc)
+                {
+                    lstCaseCompanyMapping.Add(Convert<BO.CaseCompanyMapping, CaseCompanyMapping>(item));
+                }
+                return lstCaseCompanyMapping;
+            }
+        }
+        #endregion
+
+        #region Delete By ID
+        public override object Delete(int id)
+        {
+            var acc = _context.CaseCompanyMappings.Where(p => p.Id == id
+                                     && (p.IsDeleted.HasValue == false || (p.IsDeleted.HasValue == true && p.IsDeleted.Value == false)))
+                                    .FirstOrDefault<CaseCompanyMapping>();
+            if (acc != null)
+            {
+                acc.IsDeleted = true;
+                _context.SaveChanges();
+            }
+            else if (acc == null)
+            {
+                return new BO.ErrorObject { ErrorMessage = "No record found.", errorObject = "", ErrorLevel = ErrorLevel.Error };
+            }
+
+            var res = Convert<BO.CaseCompanyMapping, CaseCompanyMapping>(acc);
+            return (object)res;
+        }
+        #endregion
+
+        #region Get By Company Id
+        public override object GetByCompanyId(int CompanyId)
+        {
+            var acc = _context.CaseCompanyMappings.Where(p => p.CompanyId == CompanyId
+                                                   && p.IsDeleted.HasValue == false || (p.IsDeleted.HasValue == true && p.IsDeleted.Value == false))
+                                                   .ToList<CaseCompanyMapping>();
+            List<BO.CaseCompanyMapping> lstCaseWithUserAndPatient = new List<BO.CaseCompanyMapping>();
+            if (acc == null)
+            {
+                return new BO.ErrorObject { ErrorMessage = "No record found for this Case.", errorObject = "", ErrorLevel = ErrorLevel.Error };
+            }
+            else
+            {
+                foreach (CaseCompanyMapping eachPatient in acc)
+                {
+                    lstCaseWithUserAndPatient.Add(Convert<BO.CaseCompanyMapping, CaseCompanyMapping>(eachPatient));
+                }
+            }
+            return lstCaseWithUserAndPatient;
         }
         #endregion
 
--- Midas/MIDAS.GBX/MIDAS.GBX.WebAPI/Controllers/CaseCompanyMappingController.cs
+++ Midas/MIDAS.GBX/MIDAS.GBX.WebAPI/Controllers/CaseCompanyMappingController.cs
@@ -21,5 +21,46 @@ namespace MIDAS.GBX.WebAPI.Controllers
             requestHandler = new GbApiRequestHandler<CaseCompanyMapping>();
         }
 
+        [HttpGet]
+        [Route("Get/{id}")]
+        [AllowAnonymous]
+        public HttpResponseMessage Get(int id)
+        {
+            return requestHandler.GetObject(Request, id);
+        }
+
+        [HttpGet]
+        [Route("getByCaseId/{CaseId}")]
+        [AllowAnonymous]
+        public HttpResponseMessage GetByCaseId(int CaseId)
+        {
+            return requestHandler.GetByCaseId(Request, CaseId);
+        }
+
+        [HttpPost]
+        [Route("Save")]
+        [AllowAnonymous]
+        public HttpResponseMessage Post([FromBody]CaseCompanyMapping data)
+        {
+            return requestHandler.CreateGbObject(Request, data);
+        }
+
+        [HttpGet]
+        [Route("Delete/{id}")]
+        [AllowAnonymous]
+        public HttpResponseMessage Delete(int id)
+        {
+            return requestHandler.Delete(Request, id);
+        }
+
+        [HttpGet]
+        [Route("getByCompanyId/{CompanyId}")]
+        [AllowAnonymous]
+        public HttpResponseMessage GetByCompanyId(int CompanyId)
+        {
+            return requestHandler.GetGbObjects(Request, CompanyId);
+        }
+
+
     }
 }
