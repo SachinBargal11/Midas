f263d3c967eff8870daa9cb039d2ab63cb6634c9
milindkate21 <mkate@greenyourbills.com>
2017-04-04 18:09:38 +0530

    Code added in patientAPI for DoctorCaseConsentApproval

---
+++ Midas/MIDAS.GBX/MIDAS.GBX.PatientWebAPI/Controllers/DoctorCaseConsentApprovalController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MIDAS.GBX.PatientWebAPI.RequestHandler;
+using MIDAS.GBX.BusinessObjects;
+
+namespace MIDAS.GBX.PatientWebAPI.Controllers
+{
+    [RoutePrefix("midaspatientapi/DoctorCaseConsentApproval")]
+
+    public class DoctorCaseConsentApprovalController : ApiController
+    {
+        private IRequestHandler<DoctorCaseConsentApproval> requestHandler;
+
+        public DoctorCaseConsentApprovalController()
+        {
+            requestHandler = new GbApiRequestHandler<DoctorCaseConsentApproval>();
+        }
+
+        [HttpGet]
+        [Route("get/{id}")]
+        [AllowAnonymous]
+        public HttpResponseMessage Get(int id)
+        {
+            return requestHandler.GetObject(Request, id);
+        }
+
+        [HttpGet]
+        [Route("getByDoctorId/{id}")]
+        [AllowAnonymous]
+        public HttpResponseMessage GetByDoctorId(int id)
+        {
+            return requestHandler.GetByDoctorId(Request, id);
+        }
+
+        [HttpGet]
+        [Route("getByCaseId/{CaseId}")]
+        [AllowAnonymous]
+        public HttpResponseMessage GetByCaseId(int CaseId)
+        {
+            return requestHandler.GetByCaseId(Request, CaseId);
+        }
+
+        [HttpPost]
+        [Route("Save")]
+        [AllowAnonymous]
+        public HttpResponseMessage Post([FromBody]DoctorCaseConsentApproval data)
+        {
+            return requestHandler.CreateGbObject(Request, data);
+        }
+
+        [HttpGet]
+        [Route("Delete/{id}")]
+        [AllowAnonymous]
+        public HttpResponseMessage Delete(int id)
+        {
+            return requestHandler.Delete(Request, id);
+        }
+
+
+    }
+}
