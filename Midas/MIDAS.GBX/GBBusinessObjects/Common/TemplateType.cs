859c5793a37f287dd1c07f66d19ced883996fc04
Sonali.A <Sonali.A@LT006>
2017-04-07 12:23:20 +0530

    TemplateType Repository and BusinessObject created

---
+++ Midas/MIDAS.GBX/GBBusinessObjects/Common/TemplateType.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIDAS.GBX.BusinessObjects.Common
+{
+   public class TemplateType : GbObject
+    {
+        [JsonProperty("templateType")]
+        public string TemplateName { get; set; }
+
+        [JsonProperty("templateText")]
+        public string TemplateText { get; set; }
+    }
+}
