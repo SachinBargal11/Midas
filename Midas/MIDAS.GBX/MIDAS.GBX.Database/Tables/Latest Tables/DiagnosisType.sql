5e968a053fd6a5137fd368c1b40d8377ce42e37d
satish.k <satish.k@codearray.tech>
2017-04-18 19:37:33 +0530

    Added new tables for [DiagnosisType] and [DiagnosisCode]

---
+++ Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/DiagnosisType.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[DiagnosisType]
+(
+	[Id] INT NOT NULL IDENTITY,
+	[DiagnosisTypeText] NVARCHAR(50) NULL, 
+    [CompanyId] INT NULL, 
+
+	[IsDeleted] [bit] NULL DEFAULT 0,
+	[CreateByUserID] [int] NOT NULL,
+	[CreateDate] [datetime2](7) NOT NULL,
+	[UpdateByUserID] [int] NULL,
+	[UpdateDate] [datetime2](7) NULL, 
+    CONSTRAINT [PK_DiagnosisType] PRIMARY KEY ([Id])
+)
+GO
+
+ALTER TABLE [dbo].[DiagnosisType]  WITH CHECK ADD  CONSTRAINT [FK_DiagnosisType_Company_CompanyId] FOREIGN KEY([CompanyId])
+	REFERENCES [dbo].[Company] ([Id])
+GO
+
+ALTER TABLE [dbo].[DiagnosisType] CHECK CONSTRAINT [FK_DiagnosisType_Company_CompanyId]
+GO
