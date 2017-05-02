80812b070e9e3860597ef34220848b024c8636bd
milindkate21 <mkate@greenyourbills.com>
2017-05-02 17:00:17 +0530

    Code added in preferredMedicalProviderAPI

--- Midas/MIDAS.GBX/MIDAS.GBX.WebAPI/Controllers/PreferredMedicalProviderController.cs
+++ Midas/MIDAS.GBX/MIDAS.GBX.WebAPI/Controllers/PreferredMedicalProviderController.cs
@@ -1,3 +1,4 @@
+using MIDAS.GBX.BusinessObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,50 @@ using System.Web.Http;
 
 namespace MIDAS.GBX.WebAPI.Controllers
 {
+    [RoutePrefix("midasapi/PreferredMedicalProvider")]
+
     public class PreferredMedicalProviderController : ApiController
     {
+        private IRequestHandler<PreferredMedicalProvider> requestHandler;
 
+        public PreferredMedicalProviderController()
+        {
+            requestHandler = new GbApiRequestHandler<PreferredMedicalProvider>();
+        }
+
+        [HttpGet]
+        [Route("associateMedicalProviderWithCompany/{PrefMedProviderId}/{CompanyId}")]
+        public HttpResponseMessage AssociateMedicalProviderWithCompany(int PrefMedProviderId, int CompanyId)
+        {
+            return requestHandler.AssociateMedicalProviderWithCompany(Request, PrefMedProviderId, CompanyId);
+        }
+
+        [HttpGet]
+        [Route("GetAllMedicalProviderExcludeAssigned/{CompanyId}")]
+        public HttpResponseMessage GetAllMedicalProviderExcludeAssigned(int CompanyId)
+        {
+            return requestHandler.GetAllMedicalProviderExcludeAssigned(Request, CompanyId);
+        }
+
+        [HttpGet]
+        [Route("Get/{CompanyId}")]
+        public HttpResponseMessage Get(int CompanyId)
+        {
+            return requestHandler.GetObject(Request, CompanyId);
+        }
+
+
+        [HttpPost]
+        [Route("Save")]
+        public HttpResponseMessage Post([FromBody]PreferredMedicalProvider data)
+        {
+            return requestHandler.CreateGbObject(Request, data);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
     }
 }
+
