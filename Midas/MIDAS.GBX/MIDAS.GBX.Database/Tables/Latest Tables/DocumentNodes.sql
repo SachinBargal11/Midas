06f9b92ec5176997a7d49fb14533b900e4c74920
Sonali.A <Sonali.A@LT006>
2017-05-03 11:51:41 +0530

    Document Table scripts added

---
+++ Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/DocumentNodes.sql
@@ -0,0 +1,9 @@
+﻿
+CREATE TABLE [dbo].[DocumentNodes](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[ParentNode] [nvarchar](10) NULL,
+	[ChildNode] [nvarchar](10) NOT NULL,
+	[NodeAbbr] [varchar](10) NULL
+) ON [PRIMARY]
+
+GO
