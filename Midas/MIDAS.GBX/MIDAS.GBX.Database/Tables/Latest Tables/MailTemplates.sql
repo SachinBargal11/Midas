2cbea493c2ac2b59296ba1095e0be97c756b9083
Vipul.K <Vipul.K@DT014>
2017-05-19 16:53:22 +0530

    Implemented notification for case add

---
+++ Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/MailTemplates.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[MailTemplates](
+	[TemplateID] [int] IDENTITY(1,1) NOT NULL,
+	[TemplateName] [nvarchar](50) NULL,
+	[EmailSubject] [nvarchar](50) NULL,
+	[EmailBody] [nvarchar](500) NULL,
+ CONSTRAINT [PK_MailTemplates] PRIMARY KEY CLUSTERED 
+(
+	[TemplateID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
+
+--insert into [dbo].[MailTemplates] values('CaseCreated','Alert Message From GBX MIDAS','Dear {0} <br><br>Case is created for patient id {1}.');
+
+Go
