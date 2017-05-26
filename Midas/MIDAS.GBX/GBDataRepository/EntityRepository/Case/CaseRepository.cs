fd4303c0fa2fa05da10679eaeff5d80b6a749d62
satish.k <satish.k@codearray.tech>
2017-05-26 15:40:51 +0530

    Get Case Companies Issue Fixed to include Company Type

--- Midas/MIDAS.GBX/GBDataRepository/EntityRepository/Case/CaseRepository.cs
+++ Midas/MIDAS.GBX/GBDataRepository/EntityRepository/Case/CaseRepository.cs
@@ -387,12 +387,9 @@ namespace MIDAS.GBX.DataRepository.EntityRepository
                 boCompany.CompanyType = (BO.GBEnums.CompanyType)company.CompanyType;
                 boCompany.SubsCriptionType = (BO.GBEnums.SubsCriptionType)company.SubscriptionPlanType;
 
-                var companyTypeDB = new CompanyType();
-                if(company.CompanyType!=0)
+                var companyTypeDB = company.CompanyType1;
+                if (company.CompanyType != 0)
                 {
-                    companyTypeDB = _context.CompanyTypes.Where(p => p.id == company.CompanyType
-                                             && (p.IsDeleted.HasValue == false || (p.IsDeleted.HasValue == true && p.IsDeleted.Value == false)))
-                                          .FirstOrDefault();
                     boCompany.CompanyType1 = new BO.CompanyType();
                     boCompany.CompanyType1.ID = companyTypeDB.id;
                     boCompany.CompanyType1.Name = companyTypeDB.Name;
@@ -983,11 +980,15 @@ namespace MIDAS.GBX.DataRepository.EntityRepository
         {
             var company1 = _context.CaseCompanyMappings.Where(p => p.CaseId == caseId
                                                             && (p.IsDeleted.HasValue == false || (p.IsDeleted.HasValue == true && p.IsDeleted.Value == false)))
-                                                       .Select(p => p.Company).ToList();
+                                                       .Select(p => p.Company)
+                                                       .Include("CompanyType1")
+                                                       .ToList();
            
             var company2 = _context.Referral2.Where(p => p.CaseId == caseId
                                                 && (p.IsDeleted.HasValue == false || (p.IsDeleted.HasValue == true && p.IsDeleted.Value == false)))
-                                             .Select(p => p.Company1).ToList();
+                                             .Select(p => p.Company1)
+                                             .Include("CompanyType1")
+                                             .ToList();
 
             if (company1 == null && company2 == null)
             {
