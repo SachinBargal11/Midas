6f75371f9864911796315a858cebb1e1862aa957
satish.k <satish.k@codearray.tech>
2017-06-02 18:09:58 +0530

    Added New Table AppMessageQueue, QueueType

---
+++ Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/AppMessageQueue.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [Messaging].[AppMessageQueue]
+(
+    [Id] INT NOT NULL IDENTITY,
+    [AppId] INT NOT NULL, 
+    [AppName] NVARCHAR(150) NOT NULL, 
+    [QueueTypeId] INT NOT NULL, 
+    CONSTRAINT [PK_AppMessageQueue] PRIMARY KEY ([Id])
+)
+GO
+
+ALTER TABLE [Messaging].[AppMessageQueue]  WITH CHECK ADD  CONSTRAINT [FK_AppMessageQueue_QueueType_QueueTypeId] FOREIGN KEY([QueueTypeId])
+	REFERENCES [Messaging].[QueueType] ([Id])
+GO
+
+ALTER TABLE [Messaging].[AppMessageQueue] CHECK CONSTRAINT [FK_AppMessageQueue_QueueType_QueueTypeId]
+GO
+
+INSERT INTO [Messaging].[AppMessageQueue] ([AppId], [AppName], [QueueTypeId]) VALUES (1, 'MIDAS', 1)
---
+++ Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/QueueType.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Messaging].[QueueType]
+(
+    [Id] INT NOT NULL IDENTITY,
+    [QueueTypeName] NVARCHAR(150) NOT NULL, 
+    CONSTRAINT [PK_QueueType] PRIMARY KEY ([Id])
+)
+GO
+
+INSERT INTO [Messaging].[QueueType] ([QueueTypeName]) VALUES ('SQL TABLE'), ('MSMQ'), ('Azure Queue'), ('Service Bus')
+
