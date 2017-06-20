3f1adfac6ebcbec339fbaf56e058860a4bf4c563
Sachin Bargal <sachin.b@codearray.tech>
2017-06-20 16:12:25 +0530

    Remove build time errors

--- web-ui-with-angular-cli/src/environments/environment.ts
+++ web-ui-with-angular-cli/src/environments/environment.ts
@@ -7,5 +7,5 @@ export const environment = {
   production: false,
   // SERVICE_BASE_URL: 'http://medicalproviderapi.codearray.tk/midasapi'
   SERVICE_BASE_URL: 'http://medicalproviderdevapi.codearray.tk/midasapi'
-  // SERVICE_BASE_URL: 'http://localhost:65184/midasapi'
+  // SERVICE_BASE_URL: 'http://caserver:7002/midasapi'
 };
