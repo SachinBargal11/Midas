c825f8730e043bb3e89391292247d3277393e29e
Vipul.K <Vipul.K@DT014>
2017-06-21 15:41:36 +0530

    Convert changes for case

--- Midas/MIDAS.GBX/GBDataRepository/EntityRepository/Case/CaseRepository.cs
+++ Midas/MIDAS.GBX/GBDataRepository/EntityRepository/Case/CaseRepository.cs
@@ -76,7 +76,7 @@ namespace MIDAS.GBX.DataRepository.EntityRepository
 
                 List<BO.CaseCompanyMapping> boCaseCompanyMapping = new List<BO.CaseCompanyMapping>();
                 foreach (var casemap in cases.CaseCompanyMappings)
-                {
+                {                  
                     if (casemap.IsDeleted.HasValue == false || (casemap.IsDeleted.HasValue == true && casemap.IsDeleted.Value == false))
                     {
                         if (casemap != null)
@@ -96,12 +96,12 @@ namespace MIDAS.GBX.DataRepository.EntityRepository
                         if (casemap.Company.CompanyType == 1)
                         {
                             caseBO.MedicalProviderId = casemap.CompanyId;
-                            caseBO.AttorneyProviderId = cases.CaseCompanyMappings.Where(p => p.AddedByCompanyId == casemap.CompanyId && p.CompanyId == 2).Select(p => p.CompanyId).FirstOrDefault();
+                            caseBO.AttorneyProviderId = cases.CaseCompanyMappings.Where(p => p.AddedByCompanyId == casemap.CompanyId && p.Company1.CompanyType == 2).Select(p => p.CompanyId).FirstOrDefault();
                         }
                         else if (casemap.Company.CompanyType == 2)
                         {
                             caseBO.AttorneyProviderId = casemap.CompanyId;
-                            caseBO.MedicalProviderId = cases.CaseCompanyMappings.Where(p => p.AddedByCompanyId == casemap.CompanyId).Select(p => p.CompanyId).FirstOrDefault();
+                            caseBO.MedicalProviderId = cases.CaseCompanyMappings.Where(p => p.AddedByCompanyId == casemap.CompanyId && p.Company1.CompanyType == 1).Select(p => p.CompanyId).FirstOrDefault();
                         }
                     }
 
