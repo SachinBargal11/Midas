01d22c3f5c3ec94343a8482e5918dc847bd2a680
Akshata KAnade <akshata.k@codearray.tech>
2017-06-22 18:41:26 +0530

    Change In URL

--- attorney-portal/src/environments/environment.prod.ts
+++ attorney-portal/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  SERVICE_BASE_URL: 'http://attorneyapi.codearray.tk/midasattorneyapi'
+  // SERVICE_BASE_URL: 'http://attorneyapi.codearray.tk/midasattorneyapi'
+  SERVICE_BASE_URL: 'http://caserver:7003/midasattorneyapi'
 };
