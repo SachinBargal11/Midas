169b165d1bd69a8602300b748145fb55be835d24
Sachin Bargal <sachin.b@codearray.tech>
2017-06-23 11:21:56 +0530

    Added all API URLs in environment.prod.ts, ignore currentCompany session for clearing session on logout

--- collection-portal/src/environments/environment.ts
+++ collection-portal/src/environments/environment.ts
@@ -6,4 +6,6 @@
 export const environment = {
   production: false,
   SERVICE_BASE_URL: 'http://medicalproviderapi.codearray.tk/midasapi'
+  // SERVICE_BASE_URL: 'http://collectionapi.codearray.tk/midascollectionapi'
+  // SERVICE_BASE_URL: 'http://collectiondevapi.codearray.tk/midascollectionapi'
 };
--- funding-portal/src/environments/environment.prod.ts
+++ funding-portal/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   SERVICE_BASE_URL: 'http://medicalproviderapi.codearray.tk/midasapi'
+  // SERVICE_BASE_URL: 'http://fundingapi.codearray.tk/midasfundingapi'
 };
