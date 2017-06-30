bea13d042ee7605e0650d07c4b24ffaf6f7af368
Sachin Bargal <sachin.b@codearray.tech>
2017-06-30 19:05:07 +0530

    Fixed issues

--- ancillary-portal/src/environments/environment.prod.ts
+++ ancillary-portal/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  SERVICE_BASE_URL: 'http://ancillaryapi.codearray.tk/midasancillaryapi'
-  // SERVICE_BASE_URL: 'http://ancillarydevapi.codearray.tk/midasancillaryapi'
+  // SERVICE_BASE_URL: 'http://ancillaryapi.codearray.tk/midasancillaryapi'
+  SERVICE_BASE_URL: 'http://ancillarydevapi.codearray.tk/midasancillaryapi'
   // SERVICE_BASE_URL: 'http://caserver:7008/midasancillaryapi'
 };
