0191ef1aa067050725a24fd1c86d77c0020df0cb
Adarsh.b <Adarsh.b@LT007>
2017-08-04 19:46:55 +0530

    Added Messeging Service Solution

---
+++ CAMessagingService/MessagingServiceManager/Entities/Enumerators.cs
@@ -0,0 +1,15 @@
+﻿namespace MessagingServiceManager.Entities
+{
+    public enum MessageDeliveryStatus
+    {
+        Scheduled = 1,
+        Delivered = 2,
+        Failed = 3
+    }
+
+    public enum DataOperationStatus
+    {
+        SavedSuccessfully = 1,
+        RecordNotExist = 2,
+    }
+}
