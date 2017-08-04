826707987bd5baba6dcbe69ca9438f9726a89fe1
Adarsh.b <Adarsh.b@LT007>
2017-08-04 19:00:42 +0530

    Updated Notification APIs

--- CANotificationService/CANotificationService/Controllers/NotificationManagerController.cs
+++ CANotificationService/CANotificationService/Controllers/NotificationManagerController.cs
@@ -10,11 +10,12 @@ using CANotificationService.Models;
 namespace CANotificationService.Controllers
 {
     //[Authorize]
+    [RoutePrefix("NotificationManager")]
     public class NotificationManagerController : ApiController
     {
-        [Route("NotificationManager/PushMessage")]
+        [Route("PushMessage")]
         [HttpPost]
-        public string PushMessage(string receiverusername, string notificationmessage, int eventid)
+        public HttpResponseMessage PushMessage(string receiverusername, string notificationmessage, int eventid)
         {
             string message = string.Empty;
 
@@ -22,18 +23,18 @@ namespace CANotificationService.Controllers
             try
             {
                 repository.AddMessage(receiverusername, notificationmessage, eventid);
-                message = "Message successfully pushed to notification service";
+                return Request.CreateResponse(HttpStatusCode.OK, "Message successfully pushed to notification service");
             }
             catch (Exception ex)
             {
                 message = "Unable to push message to notification service due error: " + ex.Message;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, message);
             }
-            return message;
         }
 
-        [Route("NotificationManager/SubscribeEvent")]
+        [Route("SubscribeEvent")]
         [HttpPost]
-        public string SubscribeEvent(string applicationname, string username, int eventid)
+        public HttpResponseMessage SubscribeEvent(string applicationname, string username, int eventid)
         {
             string message = string.Empty;
 
@@ -41,114 +42,188 @@ namespace CANotificationService.Controllers
             try
             {
                 repository.SubscribeEvent(applicationname, username, eventid);
-                message = "Notification event successfully subscribed";
+                return Request.CreateResponse(HttpStatusCode.OK, "Notification subscription successfully saved");
             }
             catch (Exception ex)
             {
                 message = "Unable subscribe notification event due error: " + ex.Message;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, message);
             }
-            return message;
         }
 
-        [Route("NotificationManager/SubscribeEvents")]
+        [Route("SubscribeEvents")]
         [HttpPost]
-        public string SubscribeEvents(string applicationname, string username, int[] eventids)
+        public HttpResponseMessage SubscribeEvents([FromBody]UserEventSubscription subscription)
         {
             string message = string.Empty;
 
             NotificationRepository repository = new NotificationRepository();
             try
             {
-                repository.SubscribeEvent(applicationname, username, eventids);
+                repository.SubscribeEvent(subscription.ApplicationName, subscription.UserName, subscription.EventIDs);
                 message = "Event(s) subscription saved successfully";
+                return Request.CreateResponse(HttpStatusCode.OK, "Notification subscription saved successfully");
             }
             catch (Exception ex)
             {
                 message = "Unable subscribe notification events due error: " + ex.Message;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, message);
             }
-            return message;
         }
 
-        [Route("NotificationManager/GetSubscriptions")]
+        [Route("GetSubscriptions")]
         [HttpGet]
-        public IEnumerable<NotificationEventSubscriptionDetail> GetSubscriptions(string applicationname, string username)
+        public HttpResponseMessage GetSubscriptions(string applicationname, string username)
         {
-            NotificationRepository repository = new NotificationRepository();
+            try
+            {
+                NotificationRepository repository = new NotificationRepository();
 
-            var subscriptions = repository.GetSubscription(applicationname, username);
-            return subscriptions;
+                var subscriptions = repository.GetSubscription(applicationname, username);
+                return Request.CreateResponse(HttpStatusCode.OK, subscriptions);
+            }
+            catch (Exception ex)
+            {
+                var message = "Unable retrieve subscription details due to erro: " + ex.Message;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, message);
+            }
         }
 
-        [Route("NotificationManager/GetSubscription")]
+        [Route("GetSubscription")]
         [HttpGet]
-        public NotificationEventSubscriptionDetail GetSubscription(string applicationname, string username, int eventid)
+        public HttpResponseMessage GetSubscription(string applicationname, string username, int eventid)
         {
-            NotificationRepository repository = new NotificationRepository();
+            try
+            {
+                NotificationRepository repository = new NotificationRepository();
 
-            var subscription = repository.GetSubscription(applicationname, username, eventid);
-            return subscription;
+                var subscription = repository.GetSubscription(applicationname, username, eventid);
+                return Request.CreateResponse(HttpStatusCode.OK, subscription);
+            }
+            catch (Exception ex)
+            {
+                var message = "Unable retrieve subscription details due to error: " + ex.Message;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, message);
+            }
         }
 
-        [Route("NotificationManager/GetApplicationGroup")]
+        [Route("GetApplicationGroup")]
         [HttpGet]
-        public NotificationApplicationGroupDetail GetApplicationGroup(string applicationName, string groupname)
+        public HttpResponseMessage GetApplicationGroup(string applicationName, string groupname)
         {
-            NotificationRepository repository = new NotificationRepository();
-            var applicationgroup = repository.GetApplicationGroup(applicationName, groupname);
-            return applicationgroup;
+            try
+            {
+                NotificationRepository repository = new NotificationRepository();
+                var applicationgroup = repository.GetApplicationGroup(applicationName, groupname);
+                return Request.CreateResponse(HttpStatusCode.OK, applicationgroup);
+            }
+            catch (Exception ex)
+            {
+                var message = "Unable retrieve application group details due to error: " + ex.Message;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, message);
+            }
         }
 
-        [Route("NotificationManager/GetGroupEventsByGroupID")]
+        [Route("GetGroupEventsByGroupID")]
         [HttpGet]
-        public IEnumerable<NotificationEventGroupDetail> GetGroupEventsByGroupID(int groupid)
+        public HttpResponseMessage GetGroupEventsByGroupID(int groupid)
         {
-            NotificationRepository repository = new NotificationRepository();
-            var eventgroups = repository.GetGroupEvent(groupid);
-            return eventgroups;
+            try
+            {
+                NotificationRepository repository = new NotificationRepository();
+                var eventgroups = repository.GetGroupEvent(groupid);
+                return Request.CreateResponse(HttpStatusCode.OK, eventgroups);
+            }
+            catch (Exception ex)
+            {
+                var message = "Unable retrieve group event details due to error: " + ex.Message;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, message);
+            }
         }
 
        
-        [Route("NotificationManager/GetGroupEvents")]
+        [Route("GetGroupEvents")]
         [HttpGet]
-        public IEnumerable<NotificationEventGroupDetail> GetGroupEvents(string applicationName, string groupname)
+        public HttpResponseMessage GetGroupEvents(string applicationName, string groupname)
         {
-            NotificationRepository repository = new NotificationRepository();
-            var eventgroups = repository.GetGroupEvent(applicationName, groupname);
-            return eventgroups;
+            try
+            {
+                NotificationRepository repository = new NotificationRepository();
+                var eventgroups = repository.GetGroupEvent(applicationName, groupname);
+                return Request.CreateResponse(HttpStatusCode.OK, eventgroups);
+            }
+            catch (Exception ex)
+            {
+                var message = "Unable retrieve group event details due to error: " + ex.Message;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, message);
+            }
         }
 
-        [Route("NotificationManager/GetMessages")]
+        [Route("GetMessages")]
         [HttpGet]
-        public List<NotificationMessage> GetMessages(string applicationname, string username)
+        public HttpResponseMessage GetMessages(string applicationname, string username)
         {
+            try
+            { 
             NotificationRepository repository = new NotificationRepository();
             var messases = repository.GetNotificationMessages(applicationname, username);
-            return messases;
+            return Request.CreateResponse(HttpStatusCode.OK, messases);
+            }
+            catch (Exception ex)
+            {
+                var message = "Unable retrieve messages due to error: " + ex.Message;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, message);
+            }
         }
 
-        [Route("NotificationManager/GetApplicationEvents")]
+        [Route("GetApplicationEvents")]
         [HttpGet]
-        public IEnumerable<NotificationEventDetail> GetApplicationEvents(string applicationName)
+        public HttpResponseMessage GetApplicationEvents(string applicationName)
         {
-            NotificationRepository repository = new NotificationRepository();
-            var events = repository.GetApplicationEvent(applicationName);
-            return events;
+            try
+            {
+                NotificationRepository repository = new NotificationRepository();
+                var events = repository.GetApplicationEvent(applicationName);
+                return Request.CreateResponse(HttpStatusCode.OK, events);
+            }
+            catch (Exception ex)
+            {
+                var message = "Unable retrieve application notification events due to error: " + ex.Message;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, message);
+            }
+
         }
 
-        [Route("NotificationManager/HasSubscriptionByEventID")]
+        [Route("HasSubscriptionByEventID")]
         [HttpGet]
-        public bool HasEventSubscription(string receiverusername, int eventid)
+        public HttpResponseMessage HasEventSubscription(string receiverusername, int eventid)
         {
+            try
+            { 
             NotificationRepository repository = new NotificationRepository();
-            return repository.HasEventSubscription(receiverusername, eventid);
+            return Request.CreateResponse(HttpStatusCode.OK, repository.HasEventSubscription(receiverusername, eventid));
+            }
+            catch (Exception ex)
+            {
+                var message = "Unable retrieve event subscription information due to error: " + ex.Message;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, message);
+            }
         }
 
-        [Route("NotificationManager/HasSubscriptionByEventName")]
+        [Route("HasSubscriptionByEventName")]
         [HttpGet]
-        public bool HasEventSubscription(string applicationname, string receiverusername, string eventname)
+        public HttpResponseMessage HasEventSubscription(string applicationname, string receiverusername, string eventname)
         {
-            NotificationRepository repository = new NotificationRepository();
-            return repository.HasEventSubscription(applicationname, receiverusername, eventname);
-        }
+            try
+            {
+                NotificationRepository repository = new NotificationRepository();
+                return Request.CreateResponse(HttpStatusCode.OK, repository.HasEventSubscription(applicationname, receiverusername, eventname));
+            }
+            catch (Exception ex)
+            {
+                var message = "Unable retrieve event subscription information due to error: " + ex.Message;
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, message);
+            }
+}
     }
 }
