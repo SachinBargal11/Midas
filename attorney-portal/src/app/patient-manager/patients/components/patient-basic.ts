44d88b405438f416d53f556f6284e7737b833db9
Ambarish Tiwari <atiwari@greenyourbills.com>
2017-08-23 16:30:11 +0530

    Added additional field in add patient in attorney portal

--- attorney-portal/src/app/patient-manager/patients/components/patient-basic.ts
+++ attorney-portal/src/app/patient-manager/patients/components/patient-basic.ts
@@ -27,6 +27,10 @@ import { environment } from '../../../../environments/environment';
 })
 
 export class PatientBasicComponent implements OnInit {
+    isEighteenOrAbove: boolean = true;
+    languagePreference: string;
+    selectedSocialMedia: any[] = [];
+    martialStatus: number;
     caseDetail: Case[];
     referredToMe: boolean = false;
     patientId: number;
@@ -88,6 +92,18 @@ export class PatientBasicComponent implements OnInit {
                     this.dateOfBirth = this.patientInfo.user.dateOfBirth
                         ? this.patientInfo.user.dateOfBirth.toDate()
                         : null;
+                    if (this.dateOfBirth) {
+                        this.calculateAge();
+                    }
+
+                    this.martialStatus = this.patientInfo.maritalStatusId;
+                    this.languagePreference = this.patientInfo.patientLanguagePreferenceMappings[0].languagePreferenceId;
+                    if(this.patientInfo.patientSocialMediaMappings.length > 0){
+                     this.selectedSocialMedia = _.map(this.patientInfo.patientSocialMediaMappings, (currentSocialMedia:any) =>{
+                        return currentSocialMedia.socialMediaId.toString();
+                    })
+                    }
+                   
                 },
                 (error) => {
                     this._router.navigate(['../'], { relativeTo: this._route });
@@ -103,14 +119,31 @@ export class PatientBasicComponent implements OnInit {
             middlename: [''],
             lastname: ['', Validators.required],
             gender: ['', Validators.required],
-            maritalStatusId: ['', Validators.required]
+            maritalStatusId: ['', Validators.required],
+            parentName: ['', Validators.required],
+            languagePreference: [''],
+            otherLanguage: [''],
+            spouseName: [''],
+            socialMedia: [''],
+
         });
 
         this.basicformControls = this.basicform.controls;
     }
 
-    ngOnInit() { 
+    ngOnInit() {
         this.url = `${this._url}/documentmanager/uploadtoblob`;
+    }
+
+    calculateAge() {
+        let now = moment();
+        let age = now.diff(this.dateOfBirth, 'years');
+        if (age < 18) {
+            this.isEighteenOrAbove = false;
+        } else {
+            this.isEighteenOrAbove = true;
+        }
+
     }
 
     onBeforeSendEvent(event) {
@@ -129,6 +162,16 @@ export class PatientBasicComponent implements OnInit {
     }
 
     savePatient() {
+        let patientSocialMediaMappings: any[] = [];
+        _.forEach(this.selectedSocialMedia, (currentSelectedSocialMedia: any) => {
+            patientSocialMediaMappings.push({
+                socialMediaId: parseInt(currentSelectedSocialMedia)
+            })
+        })
+        let patientLanguagePreferenceMappings: any[] = [];
+        patientLanguagePreferenceMappings.push({
+            languagePreferenceId: (this.languagePreference)
+        })
         this.isSavePatientProgress = true;
         let basicFormValues = this.basicform.value;
         let result;
@@ -136,6 +179,12 @@ export class PatientBasicComponent implements OnInit {
         let patient = new Patient(_.extend(existingPatientJS, {
             maritalStatusId: basicFormValues.maritalStatusId,
             updateByUserId: this._sessionStore.session.account.user.id,
+            patientLanguagePreferenceMappings: patientLanguagePreferenceMappings,
+            languagePreferenceOther: parseInt(this.languagePreference) == 3 ? basicFormValues.otherLanguage : null,
+            patientSocialMediaMappings: patientSocialMediaMappings,
+            parentOrGuardianName: !this.isEighteenOrAbove ? basicFormValues.parentName : null,
+            legallyMarried: null,
+            spouseName: parseInt(basicFormValues.maritalStatusId) == 2 ? basicFormValues.spouseName : null,
             user: new User(_.extend(existingPatientJS.user, {
                 dateOfBirth: basicFormValues.dob ? moment(basicFormValues.dob) : null,
                 firstName: basicFormValues.firstname,
