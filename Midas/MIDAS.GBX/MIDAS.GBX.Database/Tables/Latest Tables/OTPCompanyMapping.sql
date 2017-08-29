2588ad41777fe63f0cc1509d6c749f9ed2a9e9cc
satish.k <satish.k@codearray.tech>
2017-08-29 12:07:50 +0530

    Added new table [dbo].[OTPCompanyMapping]

---
+++ Midas/MIDAS.GBX/MIDAS.GBX.Database/Tables/Latest Tables/OTPCompanyMapping.sql
@@ -0,0 +1,55 @@
+﻿CREATE TABLE [dbo].[OTPCompanyMapping]
+(
+    [Id] [INT] NOT NULL IDENTITY(1,1),
+    [OTP] [NVARCHAR](12) NOT NULL, 
+    [CompanyId] [INT] NOT NULL, 
+    [ValidUntil] [DATETIME2] NOT NULL,
+    [UsedByCompanyId] [INT] NULL, 
+    [UsedAtDate] [DATETIME2] NULL, 
+    [IsCancelled] [BIT] NULL CONSTRAINT [DF_OTPCompanyMapping_IsCancelled] DEFAULT 0,
+    [IsDeleted] [BIT] NULL CONSTRAINT [DF_OTPCompanyMapping_IsDeleted] DEFAULT 0,
+	[CreateByUserID] [INT] NOT NULL,
+	[CreateDate] [DATETIME2](7) NOT NULL,
+	[UpdateByUserID] [INT] NULL,
+	[UpdateDate] [DATETIME2](7) NULL,
+    [OTPForDate] AS CAST([ValidUntil] AS DATE),
+    CONSTRAINT [PK_OTPCompanyMapping] PRIMARY KEY ([Id])--,
+    --CONSTRAINT [PK_OTPCompanyMapping_OTP_ValidUntil] UNIQUE ([OTP], [OTPForDate])
+)
+GO
+
+IF EXISTS
+(
+	SELECT	1
+	FROM	INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+	WHERE	TABLE_SCHEMA = 'dbo'
+	AND		TABLE_NAME = 'OTPCompanyMapping'
+	AND		CONSTRAINT_NAME = 'FK_OTPCompanyMapping_Company_CompanyId'
+)
+BEGIN
+	ALTER TABLE [dbo].[OTPCompanyMapping] 
+        DROP CONSTRAINT [FK_OTPCompanyMapping_Company_CompanyId]
+END
+GO
+
+ALTER TABLE [dbo].[OTPCompanyMapping] ADD CONSTRAINT [FK_OTPCompanyMapping_Company_CompanyId] FOREIGN KEY([CompanyId])
+	REFERENCES [dbo].[Company] ([Id])
+GO
+
+IF EXISTS
+(
+	SELECT	1
+	FROM	INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+	WHERE	TABLE_SCHEMA = 'dbo'
+	AND		TABLE_NAME = 'OTPCompanyMapping'
+	AND		CONSTRAINT_NAME = 'FK_OTPCompanyMapping_Company_UsedByCompanyId'
+)
+BEGIN
+	ALTER TABLE [dbo].[OTPCompanyMapping] 
+        DROP CONSTRAINT [FK_OTPCompanyMapping_Company_UsedByCompanyId]
+END
+GO
+
+ALTER TABLE [dbo].[OTPCompanyMapping] ADD CONSTRAINT [FK_OTPCompanyMapping_Company_UsedByCompanyId] FOREIGN KEY([UsedByCompanyId])
+	REFERENCES [dbo].[Company] ([Id])
+GO
