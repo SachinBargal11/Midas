d18f7396f823c3898185df04ef26bda5c39f5a84
satish.k <satish.k@codearray.tech>
2017-09-07 13:01:11 +0530

    Added Refrence IdentityServer3.AccessTokenValidation

--- Midas/MIDAS.GBX/MIDAS.GBX.PatientWebAPI/Global.asax.cs
+++ Midas/MIDAS.GBX/MIDAS.GBX.PatientWebAPI/Global.asax.cs
@@ -14,10 +14,11 @@ namespace MIDAS.GBX.PatientWebAPI
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.None;
         }
     }
 }
--- Midas/MIDAS.GBX/MIDAS.GBX.PatientWebAPI/Startup.cs
+++ Midas/MIDAS.GBX/MIDAS.GBX.PatientWebAPI/Startup.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using IdentityServer3.AccessTokenValidation;
 using Microsoft.Owin;
 using Owin;
 
@@ -12,7 +10,22 @@ namespace MIDAS.GBX.PatientWebAPI
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            var baseAddress = System.Configuration.ConfigurationManager.AppSettings.Get("AuthenticationAuthorityUrl");
+            var clientId = System.Configuration.ConfigurationManager.AppSettings.Get("ClientID");
+            var clientSecret = System.Configuration.ConfigurationManager.AppSettings.Get("Secret");
+
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = baseAddress,
+                RequiredScopes = new[] { "MidasMedicalProviderAPI", "roles", "email" },
+
+                // client credentials for the introspection endpoint
+                ClientId = clientId,
+                ClientSecret = clientSecret,
+            });
+
+            app.UseWebApi(WebApiConfig.Register());
+            //ConfigureAuth(app);
         }
     }
 }
