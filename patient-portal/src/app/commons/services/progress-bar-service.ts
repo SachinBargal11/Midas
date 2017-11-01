8e25781c921f1b8c849f811126ba2110d2fc069e
sachin <sachin.b@codearray.tech>
2017-11-01 17:48:15 +0530

    Loader css changes

--- patient-portal/src/app/commons/services/progress-bar-service.ts
+++ patient-portal/src/app/commons/services/progress-bar-service.ts
@@ -4,11 +4,14 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProgressBarService {
     counter: number = 0;
+    blockedApp = false;
     constructor() { }
     show() {
         this.counter++;
+        this.blockedApp = true;
     }
     hide() {
         this.counter = 0;
+        this.blockedApp = false;
     }
 }
