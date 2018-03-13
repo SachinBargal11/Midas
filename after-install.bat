7285f3a02e57fccb3dd062821b7cc296fdff8962
sunil-ramesh <skramesh@qwinix.io>
2018-03-13 09:14:43 +0530

    bat file added

--- after-install.bat
+++ after-install.bat
@@ -1,1 +1,1 @@
-cd Users\Administrator && aws cloudfront create-invalidation --distribution-id E3Q819PYFEAHWT --paths "/*"
+cd \Users\Administrator && aws cloudfront create-invalidation --distribution-id E3Q819PYFEAHWT --paths "/*"
