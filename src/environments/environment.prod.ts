cd2213a158196adb21fe20456e5fbde4ae416520
sunil-ramesh <skramesh@qwinix.io>
2018-03-19 15:14:00 +0530

    config and environment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   IDENTITY_SCOPE: "openid profile email roles MidasMedicalProviderAPI NotificationService MessagingServiceAPI",
-  AUTHORIZATION_SERVER_URL: "https://qa-gb-identityserver.qwinix.io/core",
+  AUTHORIZATION_SERVER_URL: "https://dev-gb-identityserver.qwinix.io/core",
   CLIENT_ID: 'MidasPortal',                  //staging production
   //CLIENT_ID: 'MidasPortalLocal',          //local
   //CLIENT_ID: 'MidasPortalDev',            //staging dev
@@ -9,7 +9,7 @@ export const environment = {
   // PATIENT_PORTAL_URI: 'http://localhost:4201/',
   // ATTORNEY_PORTAL_URI: 'http://localhost:4201/',
   // ANCILLARY_PORTAL_URI: 'http://localhost:4201/',
-  MEDICAL_PROVIDER_URI: 'http://qa-gb-medicalprovider.qwinix.io/',
+  MEDICAL_PROVIDER_URI: 'http://dev-gb-medicalprovider.qwinix.io/',
   PATIENT_PORTAL_URI: 'http://gb-patient.qwinix.io/',
   ATTORNEY_PORTAL_URI: 'http://gb-attorney.qwinix.io/',
   ANCILLARY_PORTAL_URI: 'http://gb-ancillary.qwinix.io/'
