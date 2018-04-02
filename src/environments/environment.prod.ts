88917f7cd9b58b7ebf52279516531b9db7d702a4
sandeep <sandeepv.2604@gmail.com>
2018-04-02 14:49:53 +0530

    FIrst code push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   SERVICE_BASE_URL: 'http://qa-gb-midasattorneyapi.qwinix.io/midasattorneyapi',
   IDENTITY_SERVER_URL: 'https://qa-gb-identityserver.qwinix.io',
   NOTIFICATION_SERVER_URL: 'http://qa-gb-notificationmanager.qwinix.io',
-  HOME_URL: 'http://qa.greenyourbills.qwinix.io',
+  HOME_URL: 'http://qa-greenyourbills.qwinix.io',
   APP_URL: 'http://qa-gb-attorney.qwinix.io',
   
   IDENTITY_SCOPE: "openid profile email roles MidasMedicalProviderAPI NotificationService MessagingServiceAPI",
